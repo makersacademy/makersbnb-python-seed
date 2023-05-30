c3427c65e6ffa6605a5eebd4b63ac9770e51972c
Jessie Kinganga <Jessie.kinganga@gmail.com>
2023-05-30 15:24:53 +0100

    testing push

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 
 # == Your Routes Here ==
 
+
 # GET /index
 # Returns the homepage
 # Try it:
 #   ; open http://localhost:5000/index
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
