a0d11fd789539989dde5d04df8ee6d385dd65455
ClaudiaGC1339 <claudiagonzalez1339@gmail.com>
2023-05-30 15:29:56 +0100

    created database schema and started adding seed files

---
+++ seeds/makersbnb.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS bookings;
+DROP SEQUENCE IF EXISTS bookings_id_seq;
+
+DROP TABLE IF EXISTS listings;
+DROP SEQUENCE IF EXISTS listings_id_seq;
+
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS users_id_seq;
+
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20) UNIQUE,
+    password VARCHAR(64),
+    email VARCHAR(20)
+);
+
+CREATE SEQUENCE IF NOT EXISTS listings_id_seq;
+CREATE TABLE listings (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    price FLOAT,
+    location VARCHAR(20),
+    description TEXT,
+    availability BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE IF NOT EXISTS bookings_id_seq;
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    listing_id INTEGER,
+    total FLOAT,
+    start_date DATE,
+    end_date DATE,
+    confirmed BOOLEAN,
+    FOREIGN KEY (listing_id) REFERENCES listings(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+INSERT INTO users (username, email, password) VALUES ('owner', 'owner@email.com', 'password123')
+INSERT INTO users (username, email, password) VALUES ('renter', 'renter@email.com', 'password123')
+INSERT INTO listings (user_id, price, location) VALUES (1, 150, "London")
+
+
+
+
