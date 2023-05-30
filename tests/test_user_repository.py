04cf3c4a489af3500f44ae4cd7c83ec3a7ceff26
Jasmine <j.asra.harper@gmail.com>
2023-05-30 15:24:22 +0100

    Added tests for UserRepository, sign up button and new user route

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -2,14 +2,18 @@ from lib.user_repository import UserRepository
 from lib.user import User
 
 def test_all(db_connection):
+    db_connection.seed("seeds/User_seed.sql")
     repository = UserRepository(db_connection)
     assert repository.all() == [
-        User('Sam Morgan', 'sjmog', 'samm@makersacademy.com', 'password123'),
+        User(1, 'Sam Morgan', 'sjmog', 'samm@makersacademy.com', 'password123'),
     ]
 
-def create(self, User):
-    self._connection.execute(
-        "INSERT INTO user (name, username, email, password) values (%s, %s, %s, %s)",
-        [User.name, User.username, User.email, User.password]
-    )
-    return None
+def create(db_connection):
+    db_connection.seed("seeds/user_seed.sql")
+    repository = UserRepository(db_connection)
+    user = User(2, 'Test Name', 'Test Username', 'Test Email', 'Test Password')
+    repository.create(user)
+    assert repository.all() == [
+        User(1, 'Sam Morgan', 'sjmog', 'samm@makersacademy.com', 'password123'),
+        User(2, 'Test Name', 'Test Username', 'Test Email', 'Test Password')
+        ]
