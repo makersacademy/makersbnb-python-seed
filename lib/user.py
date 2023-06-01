95a9193a537ef0a8e5cd5e444ddca2faf9d0fde8
Joe Cunliffe <122208143+Joecunliffe98@users.noreply.github.com>
2023-06-01 17:58:26 +0100

    Fixed issues with user sign up
    Fixed DB issues with user sign up as well as password validation fixes

--- lib/user.py
+++ lib/user.py
@@ -1,3 +1,5 @@
+import re
+
 class User:
     def __init__(self, id, name, email, password):
         # We initialise with all of our attributes
@@ -14,3 +16,44 @@ class User:
     # This method makes it look nicer when we print an Artist
     def __repr__(self):
         return f"User({self.id}, {self.name}, {self.email}, {self.password})"
+    
+    def is_valid(self):
+        if re.search(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b", self.email) == None:
+            return False
+        
+        if len(self.password) < 4 or re.search(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{4,}$", self.password) == None:
+            return False
+        
+        if self.name == None or self.name == "":
+            return False
+        
+        return True
+    
+    def generate_errors(self):
+        errors = []
+        if self.email == None or self.email == "":
+            errors.append("Email can't be blank")
+        elif re.search(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b", self.email) == None:
+            errors.append("Invalid email")
+        
+        if self.password == None or self.password == "":
+            errors.append("Password can't be blank")
+        elif len(self.password) < 4:
+            errors.append("Password must be at least 4 characters long")
+        
+        if  re.search(r'\d', self.password) == None:
+            errors.append("Password must contain a number")
+
+        if re.search(r'\w*[A-Z]\w*', self.password) == None:
+            errors.append("Password must contain at least 1 uppercase letter")
+
+        if len(self.password) > 0 and re.search("\\W+", self.password) == None:
+                errors.append("Password must have at least 1 special character")
+
+        if self.name == None or self.name == "":
+            errors.append("Name can't be blank")
+
+        if len(errors) > 0:
+            return ", ".join(errors)
+        else:
+            return None
