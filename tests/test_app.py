3fc5a4a71a7d71ddde4036e7681831e96e118b79
mfpsel <mfpsel@gmail.com>
2023-06-05 17:05:07 +0100

    signup added

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,7 +10,16 @@ def test_get_home(page, test_web_address):
     page.goto(f"http://{test_web_address}/")
 
     # We look at the <p> tag
-    strong_tag = page.locator("p")
+    title_tag = page.locator(".t-title")
 
     # We assert that it has the text "Welcome to MakersBNB"
-    expect(strong_tag).to_have_text("Welcome to MakersBNB")
+    expect(title_tag).to_have_text("Welcome to MakersBNB")
+
+def test_signup(page, test_web_address):
+    page.set_default_timeout(1000)
+    page.goto(f"http://{test_web_address}/")
+    page.fill("input[name=first_name]", "Test First Name")
+    page.fill("input[name=last_name]", "Test Last Name")
+    page.fill("input[name=email]", "Test Email")
+    page.fill("input[name=password]", "1234")
+    page.click("text='Sign up'")
