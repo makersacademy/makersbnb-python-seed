6995c8f3d22d5a77f4d150639ae0e91502b8fff5
Paloma Cosentino <paloma.cosentino@gmail.com>
2023-06-06 14:11:12 +0100

    Write new tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,11 +15,19 @@ def test_get_home(page, test_web_address):
     # We assert that it has the text "Welcome to MakersBNB"
     expect(title_tag).to_have_text("Welcome to MakersBNB")
 
+
+
 def test_signup(page, test_web_address):
     page.set_default_timeout(1000)
-    page.goto(f"http://{test_web_address}/signup")
+    page.goto(f"http://{test_web_address}/")
+    page.click("text='Sign-up'")
     page.fill("input[name=first_name]", "Test First Name")
     page.fill("input[name=last_name]", "Test Last Name")
     page.fill("input[name=email]", "Test Email")
     page.fill("input[name=password]", "1234")
     page.click("text='Sign up'")
+
+def test_homepage_link(page, test_web_address):
+    page.set_default_timeout(1000)
+    page.goto(f"http://{test_web_address}/signup")
+    page.click("text='Back to Homepage'")
