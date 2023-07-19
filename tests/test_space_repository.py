fe0ef419e7c48435da75c6e410d386d49a1164b6
Ethan Clawson <ethan_clawson@yahoo.co.uk>
2023-07-19 11:53:34 +0100

    adds request booking confirmation early stages and also extra links for each page

--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -4,14 +4,14 @@ def test_all_method(db_connection):
     db_connection.seed("seeds/scar_bnb.sql")
     repository = SpaceRepository(db_connection)
     spaces = repository.all()
-    assert spaces == [Space(1, 'Example bnb', 'Examply cosy bnb', 100, '18/07/23', 2)]
+    assert spaces == [Space(1, 'Example bnb', 'Examply cosy bnb', 100, '18/07/23, 19/07/23, 20/07/23', 2)]
 
 def test_create_method(db_connection):
     db_connection.seed("seeds/scar_bnb.sql")
     repository = SpaceRepository(db_connection)
     repository.create(Space(2, 'Example bnb 2', 'Examply very cosy bnb', 100, '19/07/23', 3))
     spaces = repository.all()
-    assert spaces == [Space(1, 'Example bnb', 'Examply cosy bnb', 100, '18/07/23', 2),
+    assert spaces == [Space(1, 'Example bnb', 'Examply cosy bnb', 100, '18/07/23, 19/07/23, 20/07/23', 2),
                     Space(2, 'Example bnb 2', 'Examply very cosy bnb', 100, '19/07/23', 3)]
 
 def test_find_method_works(db_connection):
@@ -19,4 +19,4 @@ def test_find_method_works(db_connection):
     repository = SpaceRepository(db_connection)
     repository.create(Space(2, 'Example bnb 2', 'Examply very cosy bnb', 100, '19/07/23', 3))
     space_found = repository.find(1)
-    assert space_found == [Space(1, 'Example bnb', 'Examply cosy bnb', 100, '18/07/23', 2)]
+    assert space_found == [Space(1, 'Example bnb', 'Examply cosy bnb', 100, '18/07/23, 19/07/23, 20/07/23', 2)]
