e8cf01220ae8024c651670b3b5bb0b4a007d6c4c
Lydia Ntafa <lydiadafa@gmail.com>
2023-07-19 16:01:10 +0100

    reftactored find_by_email on user_repo

--- tests/test_UserRepository.py
+++ tests/test_UserRepository.py
@@ -32,18 +32,11 @@ def test_create_user(db_connection):
         User(5, 'testemail', 'testpassword')
     ]
 
-def test_find_by_email_does_raise_an_error(db_connection):
+def test_find_by_email_when_doesnt_exist_returns_user_none(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
     repository = UserRepository(db_connection)
-    searched_user = repository.find_user_by_email("fahim@example.com")
-    assert searched_user == "User already exists"
-
-
-def test_find_by_email_no_error_raised(db_connection):
-    db_connection.seed("seeds/makers_bnb_database.sql")
-    repository = UserRepository(db_connection)
-    searched_user = repository.find_user_by_email("alex@example.com")
-    assert searched_user == True
+    searched_user = repository.find_by_email("obviouslywrong@example.com")
+    assert searched_user == User(None, '', '')
 
 def test_find_by_email(db_connection):
     db_connection.seed("seeds/makers_bnb_database.sql")
