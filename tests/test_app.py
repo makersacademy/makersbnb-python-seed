2f317f9d2ca6ea0e7090880e8d9ea95d510e2528
Ket Patel <ketz555@hotmail.com>
2023-10-23 21:51:47 +0100

    added homepage with testing. Edited homepage route from /index to /

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,15 +2,45 @@ from playwright.sync_api import Page, expect
 
 # Tests for your routes go here
 
+# """
+# We can render the index page
+# """
+# def test_get_index(page, test_web_address):
+#     # We load a virtual browser and navigate to the /index page
+#     page.goto(f"http://{test_web_address}/index")
+
+#     # We look at the <p> tag
+#     strong_tag = page.locator("p")
+
+#     # We assert that it has the text "This is the homepage."
+#     expect(strong_tag).to_have_text("This is the homepage.")
+
+
 """
-We can render the index page
+Get home
+Returns page with title
 """
-def test_get_index(page, test_web_address):
-    # We load a virtual browser and navigate to the /index page
+def test_request_returns_homepage(page, test_web_address):
+    page.goto(f"http://{test_web_address}/")
+    
+    strong_tag = page.locator("h2")
+
+    expect(strong_tag).to_have_text("Welcome to MakersB&B")
+
+"""
+Homepage has user sign up UI
+"""
+def test_request_homepage_has_correct_ui(page, test_web_address):
     page.goto(f"http://{test_web_address}/index")
 
-    # We look at the <p> tag
-    strong_tag = page.locator("p")
+    email_input = page.locator('input[name="email"]')
+    assert email_input.is_visible()
 
-    # We assert that it has the text "This is the homepage."
-    expect(strong_tag).to_have_text("This is the homepage.")
+    password_input = page.locator('input[name="password"]')
+    assert password_input.is_visible()
+
+    password_confirmation_input = page.locator('input[name="password_confirmation"]')
+    assert password_confirmation_input.is_visible()
+
+    submit_button = page.locator('input[type="submit"]')
+    assert submit_button.is_visible()
