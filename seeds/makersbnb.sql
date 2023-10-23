386cbb074ae33f1c05f8893c504850da7a38aea9
xengeo <xen.geo4@gmail.com>
2023-10-23 15:46:34 +0100

    Add user repository

---
+++ seeds/makersbnb.sql
@@ -0,0 +1,37 @@
+-- add tables
+-- users, spaces
+
+
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS users_id_seq;
+DROP TABLE IF EXISTS spaces;
+DROP SEQUENCE IF EXISTS spaces_id_seq;
+
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    spaces TEXT
+);
+
+
+CREATE TABLE spaces (
+    id SERIAL PRIMARY KEY,
+    space_name TEXT,
+    description TEXT,
+    price_per_night INT,
+    user_id INT
+);
+
+
+INSERT INTO users (username, spaces) VALUES ('user_1', 'space_1');
+INSERT INTO users (username, spaces) VALUES ('user_2', 'space_2');
+
+INSERT INTO 
+    spaces (space_name, description, price_per_night, user_id)
+    VALUES ('space_1', 'A nice space', 10, 1);
+INSERT INTO 
+    spaces (space_name, description, price_per_night, user_id)
+    VALUES ('space_2', 'Another nice space', 20, 2);
+
+    
