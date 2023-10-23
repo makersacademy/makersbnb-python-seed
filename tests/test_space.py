c1eb3980c6c11325781d2c664c4c0942acac2c6d
laibaali500 <alilaiba006@gmail.com>
2023-10-23 15:42:11 +0100

    Added space repo & seed

--- tests/test_space.py
+++ tests/test_space.py
@@ -2,11 +2,11 @@ from lib.space import *
 
 """
 construct space class,
-id, space_name, description, price_per_night, user_id
+id, space_name, description, price_per_night, user_id, start_date, end_date
 """
 def test_construct_space_class():
-    space = Space(1, "test_name", "test description", 30, 1)
-    assert f"{space}" == "1, test_name, test description, 30, 1"
-    space_2 = Space(1, "test_name", "test description", 30, 1)
+    space = Space(1, "test_name", "test description", 30, 1, '2023/1/1', '2023/2/1')
+    assert f"{space}" == "1, test_name, test description, 30, 1, 2023/1/1, 2023/2/1"
+    space_2 = Space(1, "test_name", "test description", 30, 1, '2023/1/1', '2023/2/1')
     assert space == space_2
 
