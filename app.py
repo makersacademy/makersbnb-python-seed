2e183f7100a782ef82d6b68ff7461a6cc1fd36dc
Zakarie <zakariaali350@gmail.com>
2023-10-23 15:11:09 +0100

    create html file and add basic data

--- app.py
+++ app.py
@@ -5,15 +5,21 @@ from lib.database_connection import get_flask_database_connection
 # Create a new Flask app
 app = Flask(__name__)
 
-# == Your Routes Here ==
 
-# GET /index
-# Returns the homepage
-# Try it:
-#   ; open http://localhost:5000/index
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+# individual page
+@app.route('/spaces/<id>', methods=['GET'])
+def get_space(id):
+    space = {
+        'name': 'Test space', 
+        'description': 'Mauris tortor enim, consequat eu faucibus vitae, imperdiet at purus. Nunc non eros non est egestas sollicitudin eget vel quam.',
+        'size': '71.2 sq. m.'
+        }
+    
+    return render_template('space.html', space=space)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
