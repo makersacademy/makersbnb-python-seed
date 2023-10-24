461c35cf51f80c17abfadba578e4202062eaeca4
Tej <muntejchana@hotmail.com>
2023-10-24 11:50:11 +0100

    status changed to approved

--- seeds/MAKERSBNB.sql
+++ seeds/MAKERSBNB.sql
@@ -7,27 +7,27 @@ id SERIAL PRIMARY KEY,
 username varchar(255),
 password varchar(255),
 email varchar(255),
-phone_number int
+phone_number text
 );
 
 CREATE TABLE spaces (
 id SERIAL PRIMARY KEY,
 name text,
 description text,
-price_per_night numeric,
-user_id int,
-constraint fk_user foreign key(id)
+price_per_night numeric (5,2),
+host_id int,
+constraint fk_user foreign key(host_id)
     references users(id)
     on delete cascade
 );
 
-"""CREATE TABLE availability (
+CREATE TABLE availability (
 id SERIAL PRIMARY KEY,
 date_not_available date,
-status boolean,
+approved boolean,
 requested_by_user_id int,
-spaces_id
-constraint fk_spaces foreign key(user_id)
-    references spaces(user_id)
+spaces_id int,
+constraint fk_spaces foreign key(spaces_id)
+    references spaces(host_id)
     on delete cascade
-);"""
+);
