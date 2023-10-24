9b626e0e8e278bfeb250c0d37400964bb3000748
Ket Patel <ketz555@hotmail.com>
2023-10-24 13:04:09 +0100

    added login page and test

--- tests/test_app.py
+++ tests/test_app.py
@@ -31,7 +31,7 @@ def test_request_returns_homepage(page, test_web_address):
 Homepage has user sign up UI
 """
 def test_request_homepage_has_correct_ui(page, test_web_address):
-    page.goto(f"http://{test_web_address}/index")
+    page.goto(f"http://{test_web_address}/")
 
     email_input = page.locator('input[name="email"]')
     assert email_input.is_visible()
@@ -41,6 +41,23 @@ def test_request_homepage_has_correct_ui(page, test_web_address):
 
     password_confirmation_input = page.locator('input[name="password_confirmation"]')
     assert password_confirmation_input.is_visible()
+
+    submit_button = page.locator('input[type="submit"]')
+    assert submit_button.is_visible()
+
+"""
+Log-in page has user log-in UI
+"""
+def test_request_login_page_has_correct_ui(page, test_web_address):
+    page.goto(f"http://{test_web_address}/login")
+    # page.get_by_role("link", name="login").click()
+    # expect (page.goto(f"http://{test_web_address}/login"))
+
+    email_input = page.locator('input[name="email"]')
+    assert email_input.is_visible()
+
+    password_input = page.locator('input[name="password"]')
+    assert password_input.is_visible()
 
     submit_button = page.locator('input[type="submit"]')
     assert submit_button.is_visible()
