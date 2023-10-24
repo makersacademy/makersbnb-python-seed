15a15a3ff43a97130d7f5fec22c37bede1e28803
fmordy01 <fmordych@gmail.com>
2023-10-24 12:50:51 +0100

    Implementing create method and tests

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -8,3 +8,9 @@ class UserRepository():
         rows = self._connection.execute('SELECT * from users')
         return [User(row['id'], row['username'], row['email'], row['password'])
                 for row in rows]
+    
+    def create(self, user):
+        self._connection.execute('INSERT INTO users (username, email, password) VALUES (%s, %s, %s)',
+                                [user.username, user.email, user.password])
+        return None
+
