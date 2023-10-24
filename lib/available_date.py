51d829c56d299c4e998b923adb75d2b831b55fb7
zitongcharliedeng <zitongcharliedeng2003@gmail.com>
2023-10-24 12:25:35 +0100

    Create and test repr method on AvailableDate

--- lib/available_date.py
+++ lib/available_date.py
@@ -1,4 +1,7 @@
 class AvailableDate: # a single available date for a particular space
-    def __init__(s, date: str, space_id):
-        s.date = date #dd/mm/yy format as a string for now
+    def __init__(s, id: int, date_name: str, space_id: int):
+        s.id = id
+        s.date_name = date_name #dd/mm/yy format as a string for now
         s.space_id = space_id
+    def __repr__(s):
+        return(f"AvailableDate({s.id}, {s.date_name}, {s.space_id})")
--- tests/test_available_date.py
+++ tests/test_available_date.py
@@ -3,10 +3,16 @@ from lib.available_date import AvailableDate
 test construction
 """
 def test_init_method():
-    first_available_date = AvailableDate("12/11/2003", 1)
-    assert first_available_date.date == "12/11/2003"
+    first_available_date = AvailableDate(2, "12/11/2003", 1)
+    assert first_available_date.id == 2
+    assert first_available_date.date_name == "12/11/2003"
     assert first_available_date.space_id == 1
 
 """
-
+test repr method
 """
+def test_repr_method(capfd):
+    first_available_date = AvailableDate(2, "12/11/2003", 1)
+    print(first_available_date)
+    out, err = capfd.readouterr()
+    assert out == "AvailableDate(2, 12/11/2003, 1)\n"
