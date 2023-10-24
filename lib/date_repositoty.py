ee53cf2dbcac6e761fa6456d068167811ebf2dae
igorlangoni <igorlangoni@gmail.com>
2023-10-24 07:35:37 -0300

    TDD CRUD methods, passed

--- lib/date_repositoty.py
+++ lib/date_repositoty.py
@@ -12,3 +12,41 @@ class DateRepository():
         return [
             Date(row['id'], str(row['date']), row['available'], row['space_id']) for row in rows
         ]
+    
+    def find_with_id(self, id):
+        rows = self._connection.execute(
+            "SELECT * FROM dates WHERE id = %s", [id]
+        )
+        row = rows[0]
+        return Date(row['id'], str(row['date']), row['available'], row['space_id'])
+    
+    def find_with_date(self, date):
+        rows = self._connection.execute(
+            "SELECT * FROM dates WHERE date = %s", [date]
+        )
+        return [
+            Date(row['id'], str(row['date']), row['available'], row['space_id']) for row in rows
+        ]
+    
+    def find_by_available(self, available):
+        rows = self._connection.execute(
+            "SELECT * FROM dates WHERE available = %s", [available]
+        )
+        return [
+            Date(row['id'], str(row['date']), row['available'], row['space_id']) for row in rows
+        ]
+    
+    def create(self, date):
+        rows = self._connection.execute(
+            "INSERT INTO dates (date, available, space_id) VALUES (%s, %s, %s) RETURNING id",
+            [date.date, date.available, date.space_id]
+        )
+        row = rows[0]
+        date.id = row['id']
+        return None
+    
+    def delete(self, id):
+        self._connection.execute(
+            "DELETE FROM dates WHERE id = %s", [id]
+        )
+        return None
--- tests/test_date_repository.py
+++ tests/test_date_repository.py
@@ -23,3 +23,74 @@ def test_all(db_connection):
         Date(9, '2023-10-02', False, 4),
         Date(10, '2023-10-03', False, 2)
     ]
+
+"""
+When calling #find with id
+I get a specific date back
+"""
+
+def test_find_with_id(db_connection):
+    db_connection.seed("seeds/makers_bnb_library.sql")
+    repo = DateRepository(db_connection)
+
+    assert repo.find_with_id(1) ==  Date(1, '2023-10-24', True, 3)
+
+def test_find_with_date(db_connection):
+    db_connection.seed("seeds/makers_bnb_library.sql")
+    repo = DateRepository(db_connection)
+
+    assert repo.find_with_date('2023-10-24') == [
+        Date(1, '2023-10-24', True, 3)
+    ]
+
+def test_find_if_available(db_connection):
+    db_connection.seed("seeds/makers_bnb_library.sql")
+    repo = DateRepository(db_connection)
+
+    assert repo.find_by_available(True) == [
+        Date(1, '2023-10-24', True, 3),
+        Date(2, '2023-10-25', True, 5),
+        Date(3, '2023-10-26', True, 2),
+        Date(4, '2023-10-27', True, 3)
+    ]
+
+def test_create(db_connection):
+    db_connection.seed("seeds/makers_bnb_library.sql")
+    repo = DateRepository(db_connection)
+
+    date = Date(None, '2023-10-07', True, 4)
+
+    repo.create(date)
+
+    assert date.id == 11
+    assert repo.all() == [
+        Date(1, '2023-10-24', True, 3),
+        Date(2, '2023-10-25', True, 5),
+        Date(3, '2023-10-26', True, 2),
+        Date(4, '2023-10-27', True, 3),
+        Date(5, '2023-10-28', False, 5),
+        Date(6, '2023-10-29', False, 1),
+        Date(7, '2023-10-30', False, 1),
+        Date(8, '2023-10-01', False, 3),
+        Date(9, '2023-10-02', False, 4),
+        Date(10, '2023-10-03', False, 2),
+        Date(11, '2023-10-07', True, 4)
+    ]
+
+def test_delete(db_connection):
+    db_connection.seed("seeds/makers_bnb_library.sql")
+    repo = DateRepository(db_connection)
+
+    repo.delete(10)
+
+    assert repo.all() == [
+        Date(1, '2023-10-24', True, 3),
+        Date(2, '2023-10-25', True, 5),
+        Date(3, '2023-10-26', True, 2),
+        Date(4, '2023-10-27', True, 3),
+        Date(5, '2023-10-28', False, 5),
+        Date(6, '2023-10-29', False, 1),
+        Date(7, '2023-10-30', False, 1),
+        Date(8, '2023-10-01', False, 3),
+        Date(9, '2023-10-02', False, 4)
+    ]
