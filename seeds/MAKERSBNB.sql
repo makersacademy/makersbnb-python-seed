0029da3a22ca60052f60cc81659d8450567e6211
Tej <muntejchana@hotmail.com>
2023-10-24 11:20:28 +0100

    working on keys

--- seeds/MAKERSBNB.sql
+++ seeds/MAKERSBNB.sql
@@ -1,36 +1,33 @@
-DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Spaces;
-DROP TABLE IF EXISTS Dates;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS spaces;
+DROP TABLE IF EXISTS availability ;
 
-CREATE TABLE Users (
-user_id SERIAL PRIMARY KEY,
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
 username varchar(255),
 password varchar(255),
 email varchar(255),
-phone_number int,
+phone_number int
 );
 
-CREATE TABLE Spaces (
-user_id SERIAL PRIMARY KEY,
-space_id SERIAL PRIMARY KEY,
+CREATE TABLE spaces (
+id SERIAL PRIMARY KEY,
 name text,
 description text,
-price_per_night int,
+price_per_night numeric,
+spaces_user_id int
+constraint fk_user foreign key(id)
+    references users(id)
+    on delete cascade
 );
 
-user_id int,
-constraint fk_user foreign key(user_id)
-    references Users(user_id)
-    on delete cascade
-
-CREATE TABLE Availability (
-space_id SERIAL PRIMARY KEY,
+"""CREATE TABLE availability (
 date_not_available date,
-approved boolean,
-);
+status boolean,
+requested_by_user_id int,
 
-space_id int,
+
 constraint fk_space foreign key(space_id)
-    references Spaces(space_id)
+    references spaces(space_id)
     on delete cascade
-
+);"""
