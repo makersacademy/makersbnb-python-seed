6b1cf5f48b60496965e129c2b980227f8e2d6399
Gina Medina <ga.medina0613@gmail.com>
2023-10-24 15:16:53 +0100

    add user complete

--- seeds/db_makers_bnb.sql
+++ seeds/db_makers_bnb.sql
@@ -68,6 +68,26 @@ VALUES (
 -- psql -h 127.0.0.1 MAKERS_BNB < seeds/db_makers_bnb.sql
 -- psql -h 127.0.0.1 MAKERS_BNB_TEST < seeds/db_makers_bnb.sql
 
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS users_id_seq;
+
+-- Then, we recreate them
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username text,
+    name text,
+    email text,
+    password text
+);
+
+INSERT INTO users (username, name, email, password) VALUES ('Amina_1', 'Amina', 'Amina@mail.com','Amina123!');
+INSERT INTO users (username, name, email, password) VALUES ('Jake_1', 'Jake', 'Jake@mail.com','Jake123!');
+INSERT INTO users (username, name, email, password) VALUES ('Sudhansh_1', 'Sudhansh', 'Sudhansh@mail.com','Sudhansh123!');
+
+-- Write file into database in terminal
+-- psql -h 127.0.0.1 MAKERS_BNB < db_makers_bnb.sql
+-- psql -h 127.0.0.1 MAKERS_BNB_TEST < db_makers_bnb.sql
 
 
 
