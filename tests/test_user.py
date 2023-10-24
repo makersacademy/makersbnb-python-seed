f6587020600432ae1d45a982b8ac181532b8218b
fmordy01 <fmordych@gmail.com>
2023-10-24 12:22:26 +0100

    Implementing eq, repr methods and tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,3 +6,12 @@ def test_construct_user_with_name_email_password():
     assert user.username == 'user1'
     assert user.email == 'user1email@example.com'
     assert user.password == 'password1'
+
+def test_if_equal():
+    user_1 = User(1,'user1', 'user1email@example.com', 'password1')
+    user_2 = User(1,'user1', 'user1email@example.com', 'password1')
+    assert user_1 == user_2
+
+def test_represent_a_string():
+    user = User(1, 'user1', 'user1email@example.com', 'password1')
+    assert str(user) == "User(1, user1, user1email@example.com, password1)"
