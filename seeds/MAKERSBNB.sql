2f385c22ebb5a09db51b336c3a896a64dd7d95ba
Tej <muntejchana@hotmail.com>
2023-10-24 10:45:58 +0100

    we don't need joins

--- seeds/MAKERSBNB.sql
+++ seeds/MAKERSBNB.sql
@@ -15,29 +15,13 @@ user_id SERIAL PRIMARY KEY,
 space_id SERIAL PRIMARY KEY,
 name text,
 description text,
-price_per_night, int,
+price_per_night int,
 );
 
 user_id int,
 constraint fk_user foreign key(user_id)
     references Users(user_id)
     on delete cascade
-
-SELECT 
-Users.user_id,
-Users.username,
-Users.email,
-Users.phone_number,
-
-Spaces.space_id,
-Spaces.user_id,
-Space.name,
-Spaces.description,
-Spaces.price_per_night,
-
-FROM Users
-    JOIN Spaces
-    ON Users.user_id = Spaces.user_id;
 
 CREATE TABLE Availability (
 space_id SERIAL PRIMARY KEY,
@@ -49,19 +33,4 @@ space_id int,
 constraint fk_space foreign key(space_id)
     references Spaces(space_id)
     on delete cascade
-
-SELECT 
-Spaces.space_id,
-Spaces.user_id,
-Space.name,
-Spaces.description,
-Spaces.price_per_night,
-
-Availability.space_id,
-Availability.date_not_available,
-Availability.approved,
-
-FROM Spaces
-    JOIN Availability
-    ON Spaces.space_id = Availability.space_id;
 
