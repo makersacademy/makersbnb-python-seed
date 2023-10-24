98fcdde333fb52dc62bee26b4857e9978933a688
Tej <muntejchana@hotmail.com>
2023-10-24 10:38:06 +0100

    doing joins

--- seeds/MAKERSBNB.sql
+++ seeds/MAKERSBNB.sql
@@ -23,9 +23,45 @@ constraint fk_user foreign key(user_id)
     references Users(user_id)
     on delete cascade
 
+SELECT 
+Users.user_id,
+Users.username,
+Users.email,
+Users.phone_number,
+
+Spaces.space_id,
+Spaces.user_id,
+Space.name,
+Spaces.description,
+Spaces.price_per_night,
+
+FROM Users
+    JOIN Spaces
+    ON Users.user_id = Spaces.user_id;
+
 CREATE TABLE Availability (
 space_id SERIAL PRIMARY KEY,
 date_not_available date,
 approved boolean,
 );
+
+space_id int,
+constraint fk_space foreign key(space_id)
+    references Spaces(space_id)
+    on delete cascade
+
+SELECT 
+Spaces.space_id,
+Spaces.user_id,
+Space.name,
+Spaces.description,
+Spaces.price_per_night,
+
+Availability.space_id,
+Availability.date_not_available,
+Availability.approved,
+
+FROM Spaces
+    JOIN Availability
+    ON Spaces.space_id = Availability.space_id;
 
