350491c2692a6d19db910bb13e5b3bdaca7f40bc
Mike Wood <142504641+Mike-W00d@users.noreply.github.com>
2023-10-25 12:37:01 +0100

    Basic Homepage Created (#2)

--- app.py
+++ app.py
@@ -17,16 +17,16 @@ connection.seed("seeds/makersbnb.sql")
 # GET /index
 # Returns the homepage
 # Try it:
-#   ; open http://localhost:5000/index
 
-
+# open http://localhost:5000/index 
+      
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
 
-
+  
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
