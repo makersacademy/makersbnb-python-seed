84712e9c8bf18c33441b4698eb4e6b509012cbc9
Lydia <lydiaratto.com>
2023-10-25 12:17:52 +0100

    updated app file with login redirect routes

--- app.py
+++ app.py
@@ -9,59 +9,16 @@ from lib.user import User
 
 # Create a new Flask app
 app = Flask(__name__)
-# TO ADD - once we have access to signed user
-# owner_id = current_user 
 
 # == Your Routes Here ==
 
-# GET /index
-# Returns the homepage
-# Try it:
-#   ; open http://localhost:5000/index
-@app.route('/index', methods=['GET'])
-def get_index():
-    return render_template('index.html')
-
-# GET /spaces/new
-# Returns the new space page with a form to add a space
-# Try it:
-#   ; open http://localhost:5000/new-space
-@app.route('/spaces/new', methods=['GET'])
-def get_new_space():
-    return render_template('new_space.html')
-
-# POST /spaces
+# GET /
 # Returns the homepage
 # Try it:
 #   ; open http://localhost:5000/
-@app.route('/spaces', methods=['POST'])
-def create_space():
-    connection = get_flask_database_connection(app)
-    repository = SpaceRepository(connection)
-
-    name = request.form['name']
-    description = request.form['description']
-    size = request.form['size']
-    price = request.form['price']
-
-    validator = SpaceParametersValidator(name, description, size, price)
-    if not validator._is_valid():
-        errors = validator.generate_errors()
-        return render_template('/spaces/new', errors=errors)
-
-    space = Space(
-        None, 
-        validator.get_valid_name(),
-        validator.get_valid_description(),
-        validator.get_valid_size(),
-        validator.get_valid_price(),
-        1 #Change last number to owner_id once we have access to current user
-    )
-
-    repository.create(space)
-    print("testing repository")
-    print(space)
-    return redirect(f'spaces/{space.id}')
+@app.route('/', methods=['GET'])
+def get_index():
+    return render_template('index.html')
 
 @app.route('/signup', methods=['GET'])
 def get_signup_page():
@@ -110,12 +67,12 @@ def login():
     if user.password != password:
         return render_template('login.html', errors='Password is incorrect')
     else:
-        return render_template('index.html') 
+        return redirect('/') 
 
-# GET /new-space
+# GET /spaces/new
 # Returns the new space page with a form to add a space
 # Try it:
-#   ; open http://localhost:5000/new-space
+#   ; open http://localhost:5000/spaces/new
 @app.route('/spaces/new', methods=['GET'])
 def get_new_space():
     return render_template('new_space.html')
