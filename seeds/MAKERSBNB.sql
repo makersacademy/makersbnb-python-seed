3cb75bd0e45cd280091af5727c5a6a6164b59588
Tej <muntejchana@hotmail.com>
2023-10-25 10:43:26 +0100

    indexing registry page, updated seed

--- seeds/MAKERSBNB.sql
+++ seeds/MAKERSBNB.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS availability ;
 CREATE TABLE users (
 id SERIAL PRIMARY KEY,
 username varchar(255),
-password varchar(255),
 name text,
+password varchar(255),
 email varchar(255),
 phone_number text
 );
