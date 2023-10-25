6824a737eb5133f32a64b7ac9f279a5e0210e3e0
mebrah <mebrahtubereket@gmail.com>
2023-10-25 13:22:23 +0100

    Adds test for new listing

--- tests/test_spaces_repository.py
+++ tests/test_spaces_repository.py
@@ -16,10 +16,9 @@ def test_list_all_space(db_connection):
     assert space1.date_from == all_listings[0].date_from
     
     
-    repository.all_listings() == [Space(1, 'Apartment 1', 'Description 1', 100, '2024-01-01', '2024-04-01'),
-                                Space(2, 'Apartment 2', 'Description 2', 200, '2024-01-01', '2024-04-01') 
-                                
-                                ]
+    assert repository.all_listings() == [Space(1, 'Apartment 1', 'Description 1', 100, datetime.date(2024, 1, 1), datetime.date(2024, 4, 1)),
+                                Space(2, 'Apartment 2', 'Description 2', 200, datetime.date(2024, 1, 1), datetime.date(2024, 4, 1)) 
+                            ]
 
 
 
@@ -28,4 +27,16 @@ add new spaces
 
 """
 
-
+def test_add_new_space_listing(db_connection):
+    db_connection.seed("seeds/spaces.sql")
+    new_listing = Space(3, 'Apartment 3', 'Description 3', 100, datetime.date(2024, 1, 1), datetime.date(2024, 2, 3))
+    repository =  SpacesRepository(db_connection)
+    repository.create_listing(new_listing)
+    assert new_listing.price == repository.all_listings()[2].price
+    assert new_listing.date_from == repository.all_listings()[2].date_from
+    assert new_listing.date_to == repository.all_listings()[2].date_to
+    assert repository.all_listings() == [
+        Space(1, 'Apartment 1', 'Description 1', 100, datetime.date(2024, 1, 1), datetime.date(2024, 4, 1)),
+        Space(2, 'Apartment 2', 'Description 2', 200, datetime.date(2024, 1, 1), datetime.date(2024, 4, 1)),
+        Space(3, 'Apartment 3', 'Description 3', 100, datetime.date(2024, 1, 1), datetime.date(2024, 2, 3))
+                            ]
