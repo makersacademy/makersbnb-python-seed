1ef924101929bc2c0426328e76f369a9b493bf84
Lizzy Pedley <lizzypedley@gmail.com>
2023-10-25 15:02:31 +0100

    Adding files for booking pages

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,9 +5,9 @@ from playwright.sync_api import Page, expect
 """
 We can render the index page
 """
-# def test_get_index(page, test_web_address):
-#     # We load a virtual browser and navigate to the /index page
-#     page.goto(f"http://{test_web_address}/index")
+def test_get_index(page, test_web_address):
+    # We load a virtual browser and navigate to the /index page
+    page.goto(f"http://{test_web_address}/index")
 
     # We look at the <p> tag
     strong_tag = page.locator("h1")
@@ -16,39 +16,39 @@ We can render the index page
     expect(strong_tag).to_have_text("Sign Up")
 
 
-# def test_get_spaces(page, test_web_address):
+def test_get_spaces(page, test_web_address):
 
-#     page.goto(f"http://{test_web_address}/spaces")
+    page.goto(f"http://{test_web_address}/spaces")
 
-#     heading = page.locator("h1")
-#     expect(heading).to_have_text("Book a space")
+    heading = page.locator("h1")
+    expect(heading).to_have_text("Book a space")
 
-#     button = page.locator('button')
-#     expect(button).to_have_text("List a space")
+    button = page.locator('button')
+    expect(button).to_have_text("List a space")
 
-#     listings = page.locator('li')
-#     expect(listings).to_have_text([
-#         'Cozy Cottage\nA charming cottage for a peaceful retreat',
-#         'Beachfront Villa\nEnjoy stunning ocean views in this luxury villa',
-#         'Mountain Chalet\nExperience the beauty of the mountains in this cozy chalet',
-#         'City Apartment\nConvenient urban living in the heart of the city',
-#         'Lakefront Cabin\nRustic cabin on the tranquil shores of the lake',
-#         'Seaside Bungalow\nRelax in a charming bungalow by the sea',
-#         'Luxury Resort Suite\nIndulge in luxury at this 5-star resort suite',
-#         'Countryside Retreat\nEscape to the peaceful countryside in this cottage',
-#         'Historic Mansion\nStep back in time at this historic mansion',
-#         'Riverside Cabin\nCozy cabin with a river view, perfect for fishing',
-#     ])
+    listings = page.locator('li')
+    expect(listings).to_have_text([
+        'Cozy Cottage\nA charming cottage for a peaceful retreat',
+        'Beachfront Villa\nEnjoy stunning ocean views in this luxury villa',
+        'Mountain Chalet\nExperience the beauty of the mountains in this cozy chalet',
+        'City Apartment\nConvenient urban living in the heart of the city',
+        'Lakefront Cabin\nRustic cabin on the tranquil shores of the lake',
+        'Seaside Bungalow\nRelax in a charming bungalow by the sea',
+        'Luxury Resort Suite\nIndulge in luxury at this 5-star resort suite',
+        'Countryside Retreat\nEscape to the peaceful countryside in this cottage',
+        'Historic Mansion\nStep back in time at this historic mansion',
+        'Riverside Cabin\nCozy cabin with a river view, perfect for fishing',
+    ])
 
-# def test_get_new(page, test_web_address):
-#     # We load a virtual browser and navigate to the /index page
-#     page.goto(f"http://{test_web_address}/spaces/new")
+def test_get_new(page, test_web_address):
+    # We load a virtual browser and navigate to the /index page
+    page.goto(f"http://{test_web_address}/spaces/new")
 
-#     # We look at the <p> tag
-#     heading = page.locator("h1")
+    # We look at the <p> tag
+    heading = page.locator("h1")
 
-#     # We assert that it has the text "This is the homepage."
-#     expect(heading).to_have_text("List a space")
+    # We assert that it has the text "This is the homepage."
+    expect(heading).to_have_text("List a space")
 
 def test_create_listing(db_connection, page, test_web_address):
     db_connection.seed("seeds/bnb.sql")
@@ -76,9 +76,6 @@ def test_create_listing(db_connection, page, test_web_address):
     title_element = page.locator("h1")
     expect(title_element).to_have_text("Book a space")
 
-   
-    # We assert that it has the text "This is the homepage."
-    expect(heading).to_have_text("Book a space")
 
 def test_signup_user(db_connection, page, test_web_address):
     db_connection.seed('seeds/bnb.sql')
@@ -90,3 +87,15 @@ def test_signup_user(db_connection, page, test_web_address):
 
     page.click("text=Sign Up")
 
+def test_listings(db_connection, page, test_web_address):
+    db_connection.seed('seeds/bnb.sql')
+    page.goto(f"http://{test_web_address}/spaces")
+
+    # sense check
+    heading = page.locator("h1")
+    expect(heading).to_have_text("Book a space")
+
+    page.click("text=Cozy Cottage\nA charming cottage for a peaceful retreat")
+
+    heading = page.locator("h1")
+    expect(heading).to_have_text("Cozy Cottage") 
--- tests/test_listing.py
+++ tests/test_listing.py
@@ -1,17 +1,18 @@
 from lib.listing import *
 
 def test_listing_init():
-    listing = Listing('test name', 'test desc', 1.99, 1)
+    listing = Listing(1, 'test name', 'test desc', 1.99, 1)
+    assert listing.id == 1
     assert listing.listing_name == 'test name'
     assert listing.listing_description == 'test desc'
     assert listing.listing_price == 1.99
     assert listing.user_id == 1
 
-def test_listing_init():
-    listing1 = Listing('test name', 'test desc', 1.99, 1)
-    listing2 = Listing('test name', 'test desc', 1.99, 1)
+def test_listing_eq():
+    listing1 = Listing(1, 'test name', 'test desc', 1.99, 1)
+    listing2 = Listing(1, 'test name', 'test desc', 1.99, 1)
     assert listing1 == listing2
 
-def test_listing_init():
-    listing = Listing('test name', 'test desc', 1.99, 1)
-    assert str(listing) == "LISTING name: test name - desc: test desc - price: 1.99 - user_id: 1"
+def test_listing_repr():
+    listing = Listing(1, 'test name', 'test desc', 1.99, 1)
+    assert str(listing) == "LISTING 1 name: test name - desc: test desc - price: 1.99 - user_id: 1"
