2b7623130ac24a1408cf584df7aed73f44e6297a
jackdench <jdench1989@gmail.com>
2023-10-26 10:40:49 +0100

    Updated DB seed to include description field for listings

--- seeds/makers_bnb.sql
+++ seeds/makers_bnb.sql
@@ -12,9 +12,9 @@ INSERT INTO users (email, password) VALUES ('test-email-1', 'c2852cf707649f8392a
 INSERT INTO users (email, password) VALUES ('test-email-2', '6eee3a5257a1329afe1dceb4ce1ebe0018e63a0788313253d3ec023228521ff2');
 INSERT INTO users (email, password) VALUES ('test-email-3', '187154941ec7f71e45f09cc85f5dc956329cee757bd1f8e1b1b2f34a4d7e0600');
 
-INSERT INTO listings (name, cost, user_id) VALUES ('first-test-listing', 45, 1);
-INSERT INTO listings (name, cost, user_id) VALUES ('second-test-listing', 70, 1);
-INSERT INTO listings (name, cost, user_id) VALUES ('third-test-listing', 35, 2);
-INSERT INTO listings (name, cost, user_id) VALUES ('fourth-test-listing', 50, 3);
-INSERT INTO listings (name, cost, user_id) VALUES ('fifth-test-listing', 100, 2);
-INSERT INTO listings (name, cost, user_id) VALUES ('sixth-test-listing', 85, 1);
+INSERT INTO listings (name, cost, description, user_id) VALUES ('first-test-listing', 'Description 1', 45, 1);
+INSERT INTO listings (name, cost, description, user_id) VALUES ('second-test-listing', 'Description 2', 70, 1);
+INSERT INTO listings (name, cost, description, user_id) VALUES ('third-test-listing', 'Description 3', 35, 2);
+INSERT INTO listings (name, cost, description, user_id) VALUES ('fourth-test-listing', 'Description 4', 50, 3);
+INSERT INTO listings (name, cost, description, user_id) VALUES ('fifth-test-listing', 'Description 5', 100, 2);
+INSERT INTO listings (name, cost, description, user_id) VALUES ('sixth-test-listing', 'Description 6', 85, 1);
