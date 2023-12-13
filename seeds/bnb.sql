74756edf1841d017de6e07f8c3af9191528a9981
Yusur <yusurmusse@hotmail.com>
2023-12-13 13:37:05 +0000

    created user table and updated availability homepage

--- seeds/bnb.sql
+++ seeds/bnb.sql
@@ -11,6 +11,23 @@ CREATE TABLE spaces (
     end_date text
 );
 
+
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS users_id_seq;
+
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username text,
+    password text
+);
+
+
 INSERT INTO spaces (name, description, price, start_date, end_date) VALUES ('Bagend', 'Hobbit Hole', 50, '25-12-23', '30-12-23');
 INSERT INTO spaces (name, description, price, start_date, end_date) VALUES ('Isengard', 'Wizards Tower', 150, '25-12-23', '30-12-23');
 INSERT INTO spaces (name, description, price, start_date, end_date) VALUES ('Minas Tirith', 'Big White City', 200, '25-12-23', '30-12-23');
+
+
+INSERT INTO users (username, password) VALUES ('user1', 'password1');
+INSERT INTO users (username, password) VALUES ('user2', 'password2');
+INSERT INTO users (username, password) VALUES ('user3', 'password3');
