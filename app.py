847b57cbae497c932f051b3c02df5d2dc0a2c4ab
Matthew East <contact@matteast.uk>
2023-12-14 12:04:49 +0000

    Updated HTML files and resolved POST and GET

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
 from lib.database_connection import get_flask_database_connection
 from lib.space_repository import SpaceRepository
 from lib.space import Space
@@ -33,17 +33,20 @@ def get_single_space(id):
     return render_template('single_space.html', space = spaces_data)
 
 
-@app.route('/new_space', methods=['POST'])
-def post_new_space():
-    connection = get_flask_database_connection(app)
-    spaces_repo = SpaceRepository(connection)
-    arg1 = request.form["name"]
-    arg2 = request.form["description"]
-    arg3 = request.form["price"]
-    arg4 = request.form["start_date"]
-    arg5 = request.form["end_date"]
-    spaces_repo.create(Space(None, arg1, arg2, arg3, arg4, arg5))
-    return ""
+@app.route('/spaces/new_space', methods=['GET', 'POST'])
+def add_new_space():
+    if request.method == 'POST':
+        connection = get_flask_database_connection(app)
+        spaces_repo = SpaceRepository(connection)
+        arg1 = request.form["name"]
+        arg2 = request.form["description"]
+        arg3 = request.form["price"]
+        arg4 = request.form["start_date"]
+        arg5 = request.form["end_date"]
+        spaces_repo.create(Space(None, arg1, arg2, arg3, arg4, arg5))
+        return redirect('/spaces/new_space')
+    else:
+        return render_template('new_space.html')
 
 
 # These lines start the server if you run this file directly
