6e2a13929609695cb8d58d6676e9098c5e4910d5
aisha <aisha_mo@hotmail.com>
2024-01-08 16:13:12 +0000

    minor change to test file

--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -1,16 +1,16 @@
 from lib.space_repository import SpaceRepository
 
 
-# test get all spaces
-def test_can_get_all_spaces():
+# # test get all spaces
+# def test_can_get_all_spaces():
 
-# test create space
-
-
-#  test find space
+# # test create space
 
 
-# test delete space
+# #  test find space
 
 
-# test update space
+# # test delete space
+
+
+# # test update space
