2d1f67f6d59864494316452b2ccd03d704925cb5
AmyMcCann <amy.mccann.34@gmail.com>
2024-01-08 13:59:13 +0000

    Created homepage and login page

--- app.py
+++ app.py
@@ -15,8 +15,33 @@ app = Flask(__name__)
 def get_index():
     return render_template('index.html')
 
+# @app.route('/test', methods=['GET'])
+# def get_test():
+#     return render_template('test.html')
+
+@app.route('/testsubmit', methods=["POST"])
+def submit_signup():
+    connection = get_flask_database_connection(app)
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    return render_template('test.html', name=name, email=email, password=password)
+# The above function gathers all of the details for sign up
+
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+@app.route('/testlogin', methods=["POST"])
+def submit_login():
+    connection = get_flask_database_connection(app)
+    email = request.form['email']
+    password = request.form['password']
+    return render_template('test_loggedin.html', email=email, password=password)
+
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+
