e2d745eba96d8b1ececebbcc2ae7c7b85ca6d996
ValerKoll <valerio.uk@gmail.com>
2024-01-08 23:51:59 +0000

    app.py, index.html updated. account.html added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os, copy
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session
 from lib.database_connection import get_flask_database_connection
 from lib.user import User
 from lib.user_repository import User_repository
@@ -7,6 +7,15 @@ from lib.space_repository import SpaceRepository
 
 # Create a new Flask app
 app = Flask(__name__)
+
+
+
+
+########################
+# SESSION pass key
+#
+app.secret_key = b"_5#2LF34nxec]"
+########################            
 
 
 # ==== Home Page - routes
@@ -21,16 +30,71 @@ def index():
 
 @app.route('/<home_page_section>', methods=['GET'])
 def index_subsection(home_page_section):
-    global active_user      #to be replaced with cookies
     _connection = get_flask_database_connection(app)
-    #
-    #
-    data = {}
+    # ..._repository = ...._repository(_connection)
+    rows = []  # fetch listed rooms:         rows = ...._repository.find(session['id'])
+    # if request = /spaces
+    
+    data = {
+        'home_page_section': home_page_section,
+        'post_list': rows
+        }
     return render_template('index.html', data=data)
 
 
 
+# ==== Account routes
+#
+#   GET arg: login
+#             register
+# 
+@app.route('/account/<section>', methods=['GET'])
+def login(section):
+    if all(['email_address' not in session and
+            any([
+                section == 'login',
+                section == 'register'
+                ])
+            ]):
+        return render_template('account.html', login_status=section)
+    elif all([
+        section == 'logout',
+        'email_address' in session
+        ]):
+        session.pop('email_address', None)
+    return redirect('/')
 
+
+@app.route('/user/<section>', methods=['POST'])
+def user(section):
+    query_result = (False, 'Generic Error')
+    _connection = get_flask_database_connection(app)
+    user_repository = User_repository(_connection)
+    if section == 'login':
+        email_address = request.form['email_address']
+        password = request.form['passcode']
+        query_result = user_repository.find(email_address, password)
+        #check for valid user entry
+        if query_result[0]:
+            session['id'] = query_result[1].id
+            session['email_address'] = query_result[1].email_address
+            return redirect('/')
+    elif section == 'register':
+        name = request.form['name']
+        email_address = request.form['email_address']
+        password = request.form['passcode']
+        query_result = user_repository.add(1, name, email_address, password)
+        if query_result[0]:
+            return redirect('/')
+    return f"WRONG: {query_result[1]}"
+
+
+
+
+# ==== Spaces routes
+#
+#   
+# 
 @app.route('/spaces/edit/<id>')
 def get_spaces(id):
     space_repo = SpaceRepository(get_flask_database_connection(app))
