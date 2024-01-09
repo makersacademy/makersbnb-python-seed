4a9e94eec15a523cb260c26b69b7d905887269e5
Holly <holly.page@imshi.co.uk>
2024-01-09 10:36:27 +0000

    user repository tests and seed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.sign_up import *
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -14,6 +15,29 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+# GET POST /sign_up
+# Requests email and password from user and stores to database
+# Try it:
+#   ; open http://localhost:5000/sign_up
+@app.route('/sign_up', methods=['GET', 'POST'])
+def index():
+    return render_template('sign_up.html')
+def sign_up():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email == "":
+        e_message = "Please fill in your email address"
+        return render_template("registration_form.html", message=e_message)
+
+    if password == "":
+        p_message = "Please fill in your password"
+        return render_template("registration_form.html", message=p_message)
+
+    # Call the create method of UserRepository to add a new user
+    UserRepository.create(email, password)
+
+    return "User registered successfully!"
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
