a2144352f36011b32f29737bf436a2c23b607047
Holly <holly.page@imshi.co.uk>
2024-01-09 11:39:11 +0000

    changed the databse name in the tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,23 +15,7 @@ def test_get_index(page, test_web_address):
     # We assert that it has the text "This is the homepage."
     expect(strong_tag).to_have_text("This is the homepage.")
 
-def test_get_logged_in_homepage(page, test_web_address):
-    # We load a virtual browser and navigate to the /index page
-    page.goto(f"http://{test_web_address}/adminlogin")
-
-    # We look at the <p> tag
-    strong_tag = page.locator("p")
-
-    # We assert that it has the text "This is the homepage."
-    expect(strong_tag).to_have_text("Logged in homepage")
-
-def test_get_login_page(page, test_web_address):
-    # We load a virtual browser and navigate to the /index page
-    page.goto(f"http://{test_web_address}/login2")
-
-    # We look at the <p> tag
-    strong_tag = page.locator("h1")
-
-    # We assert that it has the text "This is the homepage."
-    expect(strong_tag).to_have_text("Log In")
-
+def test_get_sign_up(page, test_web_address):
+    page.goto(f"http://{test_web_address}/sign_up")
+    head_tag = page.locator("h1")
+    expect(head_tag).to_have_text("MakersBnB Registration Form")
