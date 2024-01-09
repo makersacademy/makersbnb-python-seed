c4f47c1bb1575274171df587f5affdcdd1d44d49
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-01-09 11:01:14 +0000

    Adding Booking repo Test

--- lib/booking_repository.py
+++ lib/booking_repository.py
@@ -3,18 +3,18 @@ from lib.booking import Booking
 
 class BookingRepository():
     def __init__(self, connection):
-        self._connection = psycopg.connect(connection)
+        self._connection = connection
 
     def find(self, user_id):
-        result = self._connection.execute("SELECT * FROM bookings WHERE user_id = %s", user_id)
+        result = self._connection.execute("SELECT * FROM bookings WHERE user_id = %s", [user_id])
         if result:
             bookings = []
             for row in result:
-                bookings.append(Booking(row.id, row.user_id, row.date_booked))
+                bookings.append(Booking(row['id'], row['user_id'], row['space_id'], row['date_booked']))
             return bookings
         else:
             return None
     
     def create(self, booking):
-        self._connection.execute("INSERT INTO bookings (user_id, date_booked) VALUES (%s, %s) RETURNING user_id, date_booked", booking.user_id, booking.date_booked)
+        self._connection.execute("INSERT INTO bookings (user_id, space_id, date_booked) VALUES (%s, %s, %s)", [booking.user_id, booking.space_id, booking.date_booked])
         return None
