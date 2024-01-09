d6dc29bf1fba60b158ae3f904c414a2fce131874
Katie <katieannejanes@outlook.com>
2024-01-09 14:53:41 +0000

    spaces saved

--- lib/space.py
+++ lib/space.py
@@ -9,12 +9,20 @@ user_id: int"""
 
 
 class Space:
-    def __init__(self, id, space_name, description, price, user_id):
+    def __init__(self, id, space_name, description, price, user_id, start_date, end_date):
         self.id = id
         self.space_name = space_name
         self.description = description
         self.price = price
         self.user_id = user_id
+        self.start_date = start_date
+        self.end_date = end_date
+
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
+    
+    def __repr__(self):
+        return f"Space ({self.id} {self.user_id} {self.space_name} {self.description} {self.price} {self.start_date} {self.end_date})"
+    
+    
