075d4d7fb5546fced3b78b179b29c1bcb7ee6341
simonbudden <simon.budden@gmail.com>
2024-01-09 10:53:41 +0000

    Initial commit, first 2 spaces tests added, fail x2

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,15 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(strong_tag).to_have_text("This is the homepage.")
+"""
+We can navigate to the spaces page
+"""
+def test_get_spaces_page(page, test_web_address):
+    page.goto(f"http://{test_web_address}/spaces")
+
+    h1_tag = page.locator("h1")
+
+    expect(h1_tag).to_have_text("Stay at any of these spaces.")
+"""
+The spaces page contains a list of spaces
+"""
