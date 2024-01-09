30fc33dabe3c4721366c63cf89a8e8ca5820c5ce
Nick Torkington <nicktorkington@hotmail.co.uk>
2024-01-09 14:58:22 +0000

    Completed booking class and repository

--- tests/test_booking_repository.py
+++ tests/test_booking_repository.py
@@ -19,3 +19,21 @@ def test_create_booking(db_connection):
 
     assert result == [Booking(1, '2024-01-01', False, 1, 1),
                     Booking(2, '2024-02-02', False, 1, 1)]
+    
+def test_find(db_connection):
+    db_connection.seed('seeds/bnb.sql')
+    repository = BookingRepository(db_connection)
+    result = repository.find(1)
+    assert result == Booking(1, '2024-01-01', False, 1, 1)
+    
+def test_delete(db_connection):
+    db_connection.seed('seeds/bnb.sql')
+    repository = BookingRepository(db_connection)
+    repository.delete(1)
+    assert repository.all() == []
+
+def test_confirm(db_connection):
+    db_connection.seed('seeds/bnb.sql')
+    repository = BookingRepository(db_connection)
+    repository.confirm(1)
+    assert repository.find(1) == Booking(1, '2024-01-01', True, 1, 1)
