a0a6e3298f78b081a50b20cdcf69bf4d62f88ce7
Harry <harry_dickson@icloud.com>
2024-01-09 10:19:05 +0000

    First push. Create space.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
+from lib.listing import *
+from lib.listing_repository import *
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -14,6 +16,28 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+@app.route('/create', methods=['GET'])
+def get_data():
+    return render_template('create.html')
+
+@app.route('/create', methods=['POST'])
+def post_data():
+    connection = get_flask_database_connection(app)
+    repo = ListingRepository(connection)
+    title = request.form['title']
+    description = request.form['description']
+    price = request.form['price']
+    listing = Listing(None, title, description, price)
+    listing = repo.insert(listing)
+    return redirect(f'/listings/{listing.id}')
+
+@app.route('/listings/<int:id>', methods=['GET'])
+def get_listing(id):
+    connection = get_flask_database_connection(app)
+    repo = ListingRepository(connection)
+    listing = repo.select(id)
+    return render_template('listing.html', listing = listing)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
