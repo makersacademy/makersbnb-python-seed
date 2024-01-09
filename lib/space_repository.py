b4834730d0d7748f929c68e0bfffb0d05e490597
ValerKoll <valerio.uk@gmail.com>
2024-01-09 12:47:52 +0000

    db seed file implemented

--- lib/space_repository.py
+++ lib/space_repository.py
@@ -3,13 +3,23 @@ from lib.database_connection import DatabaseConnection
 
 class SpaceRepository:
     def __init__(self, connection):
-        self._conn = connection
+        self._connection = connection
     
-    def find(self, id):
-        row = self._conn.execute(f"SELECT * FROM spaces WHERE id={id}")
-        row = row[0]
-        space = Space(row['id'], row['name'], row['description'], row['price'])
-        return space
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM spaces')
+        list_to_return = []
+        for row in rows:
+            space = Space(row['id'], row['name'], row['descr'], row['price'], row['user_id'])
+            list_to_return.append(space)
+        if len(list_to_return):
+            return list_to_return
+        return False
+        
+    def find(self, id_to_find):
+        rows = self._connection.execute("SELECT * FROM spaces WHERE id=%s", [id_to_find])
+        if rows:
+            row = rows[0]
+        return Space(row['id'], row['name'], row['descr'], row['price'])
     
-    def update(self, space:Space):
-        self._conn.execute("UPDATE spaces SET name=%s, description=%s, price=%s WHERE id=%s", (space.name, space.desc, space.price, space.id))
+    def update(self, space: Space):
+        self._connection.execute("UPDATE spaces SET name=%s, descr=%s, price=%s WHERE id=%s", [space.name, space.desc, space.price, space.id])
