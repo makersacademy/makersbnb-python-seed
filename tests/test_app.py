c2edcd7d517377d3d6255fa6f0a449cc9bcbd7c3
Joe Rodgers <joerodgers89@outlook.com>
2024-01-09 16:01:01 +0000

    Added Sign Up route

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,6 +19,4 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(strong_tag).to_have_text("This is the homepage.")
-    
-def test_for_signup(page, test_web_address, db_connection):
-    pass
+
