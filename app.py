af1fd5661bd1b2b41b0f3beb5b926c440754b824
Nathan Wing <wing_nathan@hotmail.com>
2024-01-09 16:18:44 +0000

    Session functionality added.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template,redirect
+from flask import Flask, session, request, render_template, redirect, url_for, flash
 from lib.database_connection import get_flask_database_connection
 
 from lib.user import User
@@ -10,7 +10,7 @@ from lib.SpacesRepository import SpacesRepository
 
 # Create a new Flask app
 app = Flask(__name__)
-
+app.secret_key = 'your_long_and_random_secret_key'
 # == Your Routes Here ==
 
 # GET /index
@@ -25,10 +25,35 @@ def homepage():
 def get_index():
     return render_template('index.html')
 
+@app.route('/log_in', methods=['GET', 'POST'])        
+def login():                                         
+    if request.method == 'POST':                      
+        username = request.form['email']             
+        session['email'] = username                   
+        return redirect('/user')
+    return render_template('/log_in.html')
 
-@app.route('/log_in', methods=['GET'])
-def get_login():
-    return render_template('log_in.html')
+# @app.route('/log_in', methods=['GET'])
+# def login():
+    
+#     return render_template('log_in.html')
+
+# @app.route('/log_in', methods=['POST'])
+# def loginsession():
+#     if request.method == 'POST':
+#         user = request.form['email']
+#         session['user'] = user
+#         return redirect('/spaces')
+#     return render_template('/log_in.html')
+    
+@app.route("/user", methods=['GET'])
+def user():
+    if "email" in session:
+        user = session["email"]
+        
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect('/log_in')
 
 
 @app.route('/newspace', methods=['GET'])
@@ -47,8 +72,20 @@ def create_user():
     passw = request.form['passw']
     passw_conf = request.form['passw_conf']
     
+    """
+    cursor = connection.execute("SELECT email FROM users")
+    email_list = cursor.fetchall()
+    """
+    
+    """
+    if email in email_list:
+        return"E-mail already in use."
+    """
+    
+
     if passw != passw_conf:
-        return "passwords must match"
+        flash("Passwords must match.", "error")
+    
 
     # Create a user object
     user = User(None, email, passw)
@@ -58,11 +95,17 @@ def create_user():
 
 @app.route('/spaces', methods=['GET'])
 def list_spaces():
+    # user_id = session.get('user_id')
+    # if user_id is None:
+    #     return redirect('/log_in')
     connection = get_flask_database_connection(app)
     repository = SpacesRepository(connection)
     spaces = repository.list_all()
-    return render_template('spaces.html',spaces = spaces,signedin =False)
-
+    if session['email']:
+        
+       return render_template('spaces.html',spaces = spaces,signedin =True, username = session['email'])
+    else:
+       return render_template('spaces.html',spaces = spaces,signedin =False) 
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
