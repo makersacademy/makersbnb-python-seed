a820e927cd5a895739ada7fa74f7536dc6cd3eb9
Neil Uddin <neil.uddin@hotmail.com>
2024-01-09 13:05:55 +0000

    started signup

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect
 from lib.database_connection import get_flask_database_connection
-
+from lib.user import User, UserRepo
 # Create a new Flask app
 app = Flask(__name__)
-
+app.secret_key='12'
 # == Your Routes Here ==
   
 # GET /index
@@ -18,6 +18,30 @@ def get_index():
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
+
+@app.route('/', methods=['POST'])
+def signup():
+    username = request.form['username'].strip()
+    email = request.form['email'].strip()
+    password = request.form['password']
+    new_user = User(None, username, email, password, None)
+    connection = get_flask_database_connection(app)
+    user_repo = UserRepo(connection)
+    errors = user_repo.check_user(new_user)
+    if errors:
+        print(errors)
+        return redirect('/')
+    else:
+        # Save the user to the database and then go back to index page
+        user_id = user_repo.create_user(new_user) 
+        session['user_id'] = user_id
+        return render_template('spaces.html')
+    
+    
+    
+    
+    
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
 from playwright.sync_api import Page, expect
+import pytest
+from lib.user import User, UserRepo
+
 
 # Tests for your routes go here
 
@@ -6,7 +9,7 @@ from playwright.sync_api import Page, expect
 We can render the index page
 """
 
-
+@pytest.mark.skip(reason ="Skipped as not part of our testing Neil+Joe")
 def test_get_index(page, test_web_address):
     # We load a virtual browser and navigate to the /index page
     page.goto(f"http://{test_web_address}/")
@@ -16,3 +19,6 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(strong_tag).to_have_text("This is the homepage.")
+    
+def test_for_signup(page, test_web_adress, db_connection):
+    pass
