e7ccd36369cb4aa47879f0e48308d64dd111af02
JMcK4529 <07josephmckeown@gmail.com>
2024-01-10 11:06:58 +0000

    Work started on get and post routes for /booking

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.space_repository import SpaceRepository
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -14,6 +15,22 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+@app.route('/bookings', methods=['GET'])
+def get_bookings():
+    return render_template('bookings/index.html')
+
+@app.route('/bookings', methods=['POST'])
+def goto_booking():
+    space_id = request.form['id']
+    return redirect("new_booking")
+
+@app.route('/bookings/new/<int:space_id>', methods=['GET'])
+def new_booking(space_id):
+    connection = get_flask_database_connection(app)
+    space_repo = SpaceRepository(connection)
+    space = space_repo.get_space_by_id(space_id)
+    return render_template('bookings/new.html', space=space)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
