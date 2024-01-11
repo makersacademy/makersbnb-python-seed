6007ed7487544f96863d79538d4eb5ea0e88a03f
Louis Dench <louisdench12@gmail.com>
2024-01-11 12:29:15 +0000

    adds email functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from lib.user_repository import UserRepository, is_valid
+from lib.user_repository import UserRepository, hash_pass, is_valid
 from lib.spaces_repository import SpaceRepository
 from lib.spaces import Space
 import os
@@ -7,7 +7,7 @@ from lib.database_connection import get_flask_database_connection
 import jwt
 import datetime
 from functools import wraps
-from flask import request, jsonify, make_response, flash
+from flask import request, jsonify, make_response, session
 
 
 from lib.user_repository import UserRepository
@@ -15,6 +15,8 @@ from lib.spaces_repository import SpaceRepository
 from lib.spaces import Space
 from lib.booking_repository import BookingRepository
 from lib.booking import Booking
+
+from flask_mail import Mail, Message
 
 
 # Auth token generation
@@ -51,6 +53,26 @@ app = Flask(__name__)
 SECRET_KEY = os.environ.get("SECRET_KEY") or "this is a secret"
 app.config["SECRET_KEY"] = SECRET_KEY
 
+#mail config
+app.config['MAIL_SERVER']="smtp.gmail.com"
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = "MakersBnbJan2024@gmail.com"
+app.config['MAIL_PASSWORD'] = "qtwi adua ptjq bygh"
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+def send_email(subject, recipients, body):
+    email_success = False
+    msg = Message(subject)
+    msg.sender ='MakersBnbJan2024@gmail.com'
+    msg.sender=('MakersBnB', 'MakersBnbJan2024@gmail.com')
+    msg.recipients= recipients
+    msg.body = body
+    try:
+        mail.send(msg)
+    except Exception as e:
+            print(str(e))
+mail = Mail(app)
+
 # == Your Routes Here ==
 
 
@@ -72,7 +94,6 @@ def get_bookings():
 def goto_booking():
     space_id = request.form["id"]
     return redirect("new_booking")
-
 
 @app.route("/", methods=["GET", "POST"])
 def register():
@@ -111,22 +132,6 @@ def success(current_user):
 # login page
 @app.route("/login", methods=["GET", "POST"])
 def get_login():
-    # Check if user is already logged in
-    token = request.cookies.get("token")
-    if token:
-        try:
-            jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
-            # User already logged in, redirecting to spaces page
-            return redirect("/spaces")
-        except jwt.ExpiredSignatureError:
-            # Token is expired - user needs to log in again
-            # not 'required' but catches any unforeseen errors
-            flash("Your session has expired. Please log in again.", "warning")
-            return redirect("/login")
-        except:
-            # Invalid token - continue with normal login
-            return redirect("/login")
-
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -149,6 +154,7 @@ def get_login():
             response = make_response(redirect("/spaces"))
             response.set_cookie("token", token, httponly=True)
             return response
+
         else:
             return render_template("/login.html", error="Invalid credentials")
 
@@ -158,7 +164,7 @@ def get_login():
 @app.route("/logout")
 def logout():
     # Create a response object for redirection
-    response = make_response(render_template("logout.html"))
+    response = make_response(redirect("/"))
 
     # Clear the token cookie
     response.set_cookie("token", "", expires=0)
@@ -169,11 +175,10 @@ def logout():
 @app.route("/spaces", methods=["GET"])
 @token_required
 def get_all_spaces(current_user):
-    print(current_user)
     connection = get_flask_database_connection(app)
     repository = SpaceRepository(connection)
     spaces = repository.list_all_spaces()
-    return render_template("/spaces/index.html", spaces=spaces, current_user=current_user)
+    return render_template("/spaces/index.html", spaces=spaces)
 
 
 @app.route("/spaces/new", methods=["GET"])
@@ -187,8 +192,7 @@ def get_new_space(current_user):
 def create_space(current_user):
     connection = get_flask_database_connection(app)
     repository = SpaceRepository(connection)
-    user_repo = UserRepository(connection)
-    host = user_repo.username_to_id(current_user)
+    host = 1
     new_space = Space(
         None,
         request.form["name"],
@@ -215,13 +219,12 @@ def space(space_id, current_user=None):
     dates = repository.get_available_dates(space_id)
     return render_template("/spaces/space.html", space=space, dates=dates)
 
-
-@app.route("/bookings/new", methods=["POST"])
+@app.route('/bookings/new', methods=['POST'])
 @token_required
 def create_booking(current_user):
     guest_username = current_user
-    space_id = request.form["space_id"]
-    booking_date = datetime.date.fromisoformat(request.form["date_option"])
+    space_id = request.form['space_id']
+    booking_date = datetime.date.fromisoformat(request.form['date_option'])
 
     connection = get_flask_database_connection(app)
     guest_id = connection.execute(
@@ -229,82 +232,23 @@ def create_booking(current_user):
         SELECT id FROM users WHERE username=%s;
         """, [guest_username]
     )[0]['id']
+    # space_repo = SpaceRepository(connection)
+    # host_id = space_repo.get_space_by_id(space_id).host_id
     new_booking = Booking(None, booking_date, space_id, guest_id, None)
     booking_repo = BookingRepository(connection)
     booking_repo.create(new_booking)
-    # Commented lines below may later be useful for notification sending to host
-    # space_repo = SpaceRepository(connection)
-    # host_id = space_repo.get_space_by_id(space_id).host_id
-    # user_repo = UserRepository(connection)
-    # host_username = user_repo.id_to_username(host_id)
     return redirect("/bookings/success")
 
-
-@app.route("/bookings/success", methods=["GET"])
+@app.route('/bookings/success', methods=['GET'])
 @token_required
 def get_bookings_success(current_user):
     return render_template("bookings/success.html")
 
-@app.route('/requests', methods=['GET'])
-@token_required
-def get_requests(current_user):
-    connection = get_flask_database_connection(app)
-    user_repo = UserRepository(connection)
-    user_id = user_repo.username_to_id(current_user)
-    # Find all bookings where user.id is the guest_id
-    booking_repo = BookingRepository(connection)
-    requests_made = booking_repo.find_by_guest_id(user_id)
-    # Find all spaces where user.id is the host_id AND
-    # find all bookings where space_id is in this list
-    space_repo = SpaceRepository(connection)
-    host_spaces = space_repo.get_spaces_by_host_id(user_id)
-    requests_received = []
-    for space in host_spaces:
-        for booking in booking_repo.find_by_space_id(space.id):
-            requests_received.append(booking)
-    return render_template("requests/index.html",
-                           requests_made=requests_made,
-                           requests_received=requests_received)
 
-@app.route('/requests/<int:booking_id>', methods=['GET'])
-@token_required
-def get_request_by_id(booking_id, current_user):
-    connection = get_flask_database_connection(app)
-    booking_repo = BookingRepository(connection)
-    booking = booking_repo.find(booking_id)
-    guest_id = booking.guest_id
-    user_repo = UserRepository(connection)
-    guest_username = user_repo.id_to_username(guest_id)
-    space_id = booking.space_id
-    space_repo = SpaceRepository(connection)
-    space = space_repo.get_space_by_id(space_id)
-    host_username = user_repo.id_to_username(space.host_id)
-    current_user_id = user_repo.username_to_id(current_user)
-
-    return render_template("bookings/booking.html",
-                           current_user_id=current_user_id,
-                           space=space,
-                           host_username=host_username,
-                           guest_username=guest_username,
-                           booking=booking)
-
-@app.route('/bookings/confirm', methods=['POST'])
-@token_required
-def post_confirm_booking(current_user):
-    booking_id = request.form['booking_id']
-    connection = get_flask_database_connection(app)
-    booking_repo = BookingRepository(connection)
-    booking_repo.confirm(int(booking_id))
-    return redirect(f"/requests/{booking_id}")
-
-@app.route('/bookings/reject', methods=['POST'])
-@token_required
-def post_reject_booking(current_user):
-    booking_id = int(request.form['booking_id'])
-    connection = get_flask_database_connection(app)
-    booking_repo = BookingRepository(connection)
-    booking_repo.reject(booking_id)
-    return redirect(f"/requests/{booking_id}")
+@app.route("/send_mail")
+def email_test():
+    send_email('test subject', ['louisdench12@gmail.com'], "test\nhello")
+    return "email sent"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 3000)))
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
