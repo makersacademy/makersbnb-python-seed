5d8502a11381e0547962763aa3bd6feaf67bf2b9
JMcK4529 <07josephmckeown@gmail.com>
2024-01-12 14:57:27 +0000

    Start end-to-end space and booking test

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,14 +13,17 @@ class Setup:
     def __init__(self):
         pass
 
-    def register(self, page, test_web_address):
+    def register(self, page, test_web_address,
+                 user_details={"username": "TestUser4",
+                               "email": "testuser4@test.com",
+                               "password": "TestUser4pass!"}):
         page.goto(f"http://{test_web_address}/")
         inputs = page.locator("input").all()
         values = [
-            "TestUser4",
-            "testuser@test.com",
-            "TestUser4pass!",
-            "TestUser4pass!"
+            user_details['username'],
+            user_details['email'],
+            user_details['password'],
+            user_details['password']
         ]
         for input, value in zip(inputs[:-1], values):
             input.fill(value)
@@ -28,13 +31,23 @@ class Setup:
         page.locator("a").all()[-1].click()
         return page
     
-    def login(self, page, test_web_address):
-        page = setup.register(page, test_web_address)
+    def login(self, page, test_web_address,
+              user_details={"username": "TestUser4",
+                               "email": "testuser4@test.com",
+                               "password": "TestUser4pass!"}):
         inputs = page.locator("input").all()
-        values = ["TestUser4", "TestUser4pass!"]
+        values = [user_details['username'], user_details['password']]
         for input, value in zip(inputs[:-1], values):
             input.fill(value)
         inputs[-1].click()
+        return page
+    
+    def logout(self, page):
+        links = page.locator("a").all()
+        for link in links:
+            if "Logout" in link.inner_text():
+                link.click()
+                break
         return page
 
 setup = Setup()
@@ -78,6 +91,7 @@ def test_get_login(test_web_address, db_connection, browser):
 def test_logout(test_web_address, db_connection, browser):
     page = browser.new_page()
     db_connection.seed("seeds/makersbnb.sql")
+    page = setup.register(page, test_web_address)
     page = setup.login(page, test_web_address)
     links = page.locator("a").all()
     for link in links:
@@ -97,6 +111,7 @@ def test_logout(test_web_address, db_connection, browser):
 def test_get_all_spaces(test_web_address, db_connection, browser):
     page = browser.new_page()
     db_connection.seed("seeds/makersbnb.sql")
+    page = setup.register(page, test_web_address)
     page = setup.login(page, test_web_address)
     expect(page).to_have_url(f"http://{test_web_address}/spaces")
     h2_tags = page.locator("h2").all()
@@ -105,6 +120,7 @@ def test_get_all_spaces(test_web_address, db_connection, browser):
 def test_visit_a_space(test_web_address, db_connection, browser):
     page = browser.new_page()
     db_connection.seed("seeds/makersbnb.sql")
+    page = setup.register(page, test_web_address)
     page = setup.login(page, test_web_address)
     a_tags = page.locator("a").all()
     for tag in a_tags:
@@ -116,6 +132,7 @@ def test_visit_a_space(test_web_address, db_connection, browser):
 def test_list_a_new_space(test_web_address, db_connection, browser):
     page = browser.new_page()
     db_connection.seed("seeds/makersbnb.sql")
+    page = setup.register(page, test_web_address)
     page = setup.login(page, test_web_address)
     buttons = page.locator(".button").all()
     for button in buttons:
@@ -149,6 +166,7 @@ We can render a requests index page
 def test_get_bookings(test_web_address, db_connection, browser):
     page = browser.new_page()
     db_connection.seed("seeds/makersbnb.sql")
+    page = setup.register(page, test_web_address)
     page = setup.login(page, test_web_address)
     a_tags = page.locator("a").all()
     for tag in a_tags:
@@ -156,6 +174,44 @@ def test_get_bookings(test_web_address, db_connection, browser):
             tag.click()
             break
     expect(page).to_have_url(f"http://{test_web_address}/requests")
+
+def test_make_and_view_bookings(test_web_address, db_connection, browser):
+    host_details = {"username": "NewHostUser1",
+                    "email": "newhostuser1@test.com",
+                    "password": "NewHostUser1pass!"}
+    guest_details = {"username": "NewGuestUser1",
+                     "email": "newguestuser1@test.com",
+                     "password": "NewGuestUser1pass!"}
+    page = browser.new_page()
+    db_connection.seed("seeds/makersbnb.sql")
+    page = setup.register(page, test_web_address, user_details=host_details)
+    page = setup.login(page, test_web_address, user_details=host_details)
+    buttons = page.locator(".button").all()
+    for button in buttons:
+        if button.get_attribute("href") == "spaces/new":
+            button.click()
+            break
+    expect(page).to_have_url(f"http://{test_web_address}/spaces/new")
+    all_inputs = page.locator("input").all()
+    list_space_inputs = []
+    for input in all_inputs:
+        if input.get_attribute("name") in ["name", 
+                                           "description", 
+                                           "pricePerNight", 
+                                           "availableFrom", 
+                                           "availableTo"]:
+            list_space_inputs.append(input)
+        if input.get_attribute("value") == "List my Space":
+            submit = input
+    values = ["64 Zoo Lane", "Pet friendly detached house.", "25.00", "2024-02-01", "2024-02-05"]
+    for input, value in zip(list_space_inputs, values):
+        input.fill(value)
+    submit.click()
+    expect(page).to_have_url(f"http://{test_web_address}/spaces")
+    space_button_texts = [tag.inner_text()
+                     for tag in page.locator(".spacebutton").all()]
+    assert "64 Zoo Lane" in space_button_texts
+
 
 # """
 # We can navigate to the booking page for a given space using /bookings POST
@@ -167,14 +223,6 @@ def test_get_bookings(test_web_address, db_connection, browser):
 #     page.get_by_label("Space ID:").fill("1")
 #     page.get_by_role("button").click()
 #     expect(page).to_have_url(f"http://{test_web_address}/bookings/new/1")
-
-# """
-# We can render a page for creating new bookings
-# """
-# @pytest.mark.skip(reason="Test not written")
-# def test_get_bookings_new(page, test_web_address, db_connection):
-#     db_connection.seed("seeds/makersbnb.sql")
-#     page.goto(f"http://{test_web_address}/bookings/new/1")
 
 # """
 # We can create a new booking using the new booking form
