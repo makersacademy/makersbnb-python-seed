b01e0f5b67dd193ff1425e906dcdaa2c3722b0fb
samneat <samjneat@gmail.com>
2024-03-04 14:58:12 +0000

    create routes and test render correct templates

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,29 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(strong_tag).to_have_text("This is the homepage.")
+
+
+"""
+We can render the login page
+"""
+def test_get_login(page, test_web_address):
+    page.goto(f"http://{test_web_address}/login")
+    h1 = page.locator("h1")
+    expect(h1).to_have_text("This is login")
+
+
+"""
+We can render sign up page
+"""
+def test_get_sign_up(page, test_web_address):
+    page.goto(f"http://{test_web_address}/sign_up")
+    h1 = page.locator("h1")
+    expect(h1).to_have_text("This is sign up")
+
+"""
+We can render spaces page
+"""
+def test_get_spaces(page, test_web_address):
+    page.goto(f"http://{test_web_address}/spaces")
+    h1 = page.locator('h1')
+    expect(h1).to_have_text("SPACES")
