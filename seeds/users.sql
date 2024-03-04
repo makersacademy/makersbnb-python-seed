6c39aacb1ec525f0c651a952d4bd907ed3b131f0
samneat <samjneat@gmail.com>
2024-03-04 15:34:18 +0000

    created dev_database and user seed

---
+++ seeds/users.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS users;
+
+-- Then, we recreate them
+CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255));
+
+-- Finally, we add any records that are needed for the tests to run
+INSERT INTO users (name, email, password) VALUES ('user1', 'user1@example.com', 'abc123');
+INSERT INTO users (name, email, password) VALUES ('user2', 'user2@yahoo.com', 'password123');
+INSERT INTO users (name, email, password) VALUES ('user3', 'user3@gmail.com', 'letmein!');
