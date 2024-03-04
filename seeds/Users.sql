75306c3c886235d8d3af23812cfbc19a4858d631
Maria Jose <mj_hernandezdelprado@hotmail.com>
2024-03-04 15:25:02 +0000

    change table name

--- seeds/Users.sql
+++ seeds/Users.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS "user";
-DROP SEQUENCE IF EXISTS user_id_seq;
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS users_id_seq;
 
 -- Then, we recreate them
-CREATE SEQUENCE IF NOT EXISTS user_id_seq;
-CREATE TABLE "user" (
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     password VARCHAR(255),
@@ -11,8 +11,8 @@ CREATE TABLE "user" (
 );
 
 -- Finally, we add any records that are needed for the tests to run
-INSERT INTO "user" (name, password, email) VALUES ('Maria', 'Password', 'maria@gmail.com');
-INSERT INTO "user" (name, password, email) VALUES ('Aakash', 'P*ssword', 'Aakash@outllok.com');
-INSERT INTO "user" (name, password, email) VALUES ('Reeva', 'Pass1234', 'Reeva@yahoo.com');
-INSERT INTO "user" (name, password, email) VALUES ('Jess', '1234Passw','Jess@hotmail.com');
-INSERT INTO "user" (name, password, email) VALUES ('George', '**Passwo','George@gmail.com');
+INSERT INTO users (name, password, email) VALUES ('Maria', 'Password', 'maria@gmail.com');
+INSERT INTO users (name, password, email) VALUES ('Aakash', 'P*ssword', 'Aakash@outllok.com');
+INSERT INTO users (name, password, email) VALUES ('Reeva', 'Pass1234', 'Reeva@yahoo.com');
+INSERT INTO users (name, password, email) VALUES ('Jess', '1234Passw','Jess@hotmail.com');
+INSERT INTO users (name, password, email) VALUES ('George', '**Passwo','George@gmail.com');
