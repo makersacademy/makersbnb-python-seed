e28850fad8615d1edce94b16d2ba31230c40bcef
Bogdan <bogdans@me.com>
2024-03-04 16:26:57 +0000

    bogdan pair work - added user values to SQL and create UserRepo class and tests (all pass)

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -6,26 +6,54 @@ from lib.user import User
 when we call all,
 we get a list of all users on the database
 """
+# come back and add database name 
 
 def test_get_all_users(db_connection):
-    db_connection.seed("ADD THIS")
+    db_connection.seed("seeds/makersbnb_test.sql")
     repo = UserRepository(db_connection)
 
     users = repo.all()
 
     assert users == [
-        User(1, "user1@test.com", "password123")
+        User(1, "user1@test.com", "password123"),
+        User(2, "user2@test.com", "password000")
     ]
 
 """
-when a user is created
+when a single user is created
 we can retrieve it from the database
 """
 
 def test_add_user(db_connection):
-    db_connection.seed("makersbnb_test")
-    user = User(db_connection)
+    db_connection.seed("seeds/makersbnb_test.sql")
+    user = UserRepository(db_connection)
 
-    user.add(User(None, "user1", "password123"))
-    result = user.id(1)
-    assert result == User(1, "user1", "password123")
+    user.create(User(None, "user3@test.com", "password456"))
+    result = user.all()
+    
+    assert result == [
+        User(1, "user1@test.com", "password123"),
+        User(2, "user2@test.com", "password000"),
+        User(3, "user3@test.com", "password456"),
+    ]
+
+
+"""
+when multiple users are created
+we can retrieve them from the database
+"""
+
+def test_add_users(db_connection):
+    db_connection.seed("seeds/makersbnb_test.sql")
+    user = UserRepository(db_connection)
+
+    user.create(User(None, "user3@test.com", "password456"))
+    user.create(User(None, "user4@test.com", "password999"))
+    result = user.all()
+
+    assert result == [
+        User(1, "user1@test.com", "password123"),
+        User(2, "user2@test.com", "password000"),
+        User(3, "user3@test.com", "password456"),
+        User(4, "user4@test.com", "password999"),
+    ]
