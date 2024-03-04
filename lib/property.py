ae9f9472c631483c770dc8f27f68b8bebff33049
Reeva <reevalanise@gmail.com>
2024-03-04 13:19:03 +0000

    Added Property Class

---
+++ lib/property.py
@@ -0,0 +1,17 @@
+class Property:
+    def __init__(self, name, description, price, date, owner, booked_status):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.date = date 
+        self.owner = owner #possibly user_id
+        self.booked_status = booked_status
+        
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+    
+    def __repr__(self):
+        return f"{self.name}\n {self.description}\n £{self.price}\n Owner: {self.owner}"
+        
+        
+        
