f28fc16b390d238053b40336a15592cb1f52316a
Robert <robertastalker@gmail.com>
2024-03-05 17:07:35 +0000

    Date refactoring

--- lib/space.py
+++ lib/space.py
@@ -2,13 +2,15 @@ from datetime import datetime
 import pandas as pd
 
 class Space:
-    def __init__(self, id, description, price, user_id, name, fromdate, todate):
+    def __init__(self, id, description, price, user_id, name, fromdate, todate, free_dates=[]):
         self.id = id
         self.description = description
         self.price = price
         self.user_id = user_id
         self.name = name
-        self.free_dates = pd.date_range(start=fromdate,end=todate)
+        self.fromdate = fromdate
+        self.todate = todate
+        self.free_dates = pd.date_range(start=self.fromdate,end=self.todate)
         self.free_dates = self.free_dates.strftime('%d-%m-%Y').tolist()
 
 
@@ -16,5 +18,5 @@ class Space:
         return self.__dict__ == other.__dict__
 
     def __repr__(self):
-        return f'Space({self.id}, {self.description}, {self.price}, {self.user_id}, {self.name}, {self.free_dates})'
+        return f'Space({self.id}, {self.description}, {self.price}, {self.user_id}, {self.name}, {self.fromdate}, {self.todate}, {self.free_dates})'
 
