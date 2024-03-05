8c719fbf441ea8132929775b17af1f7029eed237
Henry Stone <henrystone@live.com>
2024-03-05 17:33:08 +0000

    first draft

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -15,10 +15,24 @@ class UserRepository:
     #         artists.append(item)
     #     return artists
 
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM users')
+        users = []
+        for row in rows:
+            user = User(row['id'], row['name'], row['email'], row['password'], row['user_id'])
+            users.append(user)
+        return 
 
     def find(self, name, password):
         record = self._connection.execute('SELECT EXISTS (SELECT * FROM users WHERE name = %s AND password = %s)', (name, password))
         return record[0].get('exists')
 
+    def create(self, name, email, password):
+        self._connection.execute('INSERT INTO users (name, email, password) VALUES (%s, %s, %s)', [name, email, password])
 
+    def update(self, user):
+        self._connection.execute('UPDATE users SET name = %s, email = %s, password = %s WHERE id = %s', [user.name, user.email, user.password, user.id])
+
+    def delete(self, id):
+        self._connection.execute('DELETE FROM users WHERE id = %s', [id])
 
