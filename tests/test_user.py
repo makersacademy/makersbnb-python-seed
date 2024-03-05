9fb1c1b00c7d9df2c73bc44dafe3110dfa946785
Adam Takac <adam.takac13@gmail.com>
2024-03-05 15:54:40 +0000

    added user.py  and test_user.py

---
+++ tests/test_user.py
@@ -0,0 +1,44 @@
+from lib.user import *
+import pytest
+
+"""
+Given both password and username are valid, no exception is raised
+"""
+def test_with_valid_password_and_username():
+    user = User('adam.email@gmail.com', 'Password123!')
+    assert user.username == 'adam.email@gmail.com'
+    assert user.password == 'Password123!'
+
+"""
+Given the password is too short, a ValueError is raised
+"""
+def test_with_short_password():
+    with pytest.raises(ValueError) as e:
+        User('adam.email@gmail.com', 'Short1!')
+    error_message = str(e.value)
+    assert error_message == "Password does not meet the criteria, password needs to be 8 characters long and contain special character"
+
+
+"""
+Given the username is an invalid email, a ValueError is raised
+"""
+def test_with_invalid_email():
+    with pytest.raises(ValueError) as exc_info:
+        User('invalid-email', 'Password123!')
+    assert "Invalid username format, enter your email address"
+
+"""
+Given the password lacks special characters, a ValueError is raised
+"""
+def test_with_password_missing_special_characters():
+    with pytest.raises(ValueError) as exc_info:
+        User('adam.email@gmail.com', 'Password123')
+    assert "Password does not meet the criteria, password needs to be 8 characters long and contain special character" 
+
+"""
+Given the password is valid but the email is invalid, a ValueError is raised
+"""
+def test_with_valid_password_invalid_email():
+    with pytest.raises(ValueError) as exc_info:
+        User('adam.email', 'Password123!')
+    assert "Invalid username format, enter your email address" 
