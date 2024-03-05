6107acf06ee684148d1fd66d6b64bce191840937
Henry Stone <henrystone@live.com>
2024-03-05 17:51:51 +0000

    commit

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -26,3 +26,22 @@ def test_find_record_correct_in_different_records(db_connection):
     user = repo.find("user2", "abc123")
     assert user == False
 
+
+
+# def test_all(db_connection):
+#     db_connection.seed("seeds/users.sql")
+#     repo = UserRepository(db_connection)
+#     all_users = repo.all()
+#     assert all_users == [User(1, 'user1', 'user1@example.com', 'abc123'), User(2, 'user2', 'user2@yahoo.com', 'password123'), User(3, 'user3', 'user3@gmail.com', 'letmein!')]
+
+# def test_all(db_connection):
+#     db_connection.seed("seeds/users.sql")
+#     pass
+
+# def test_all(db_connection):
+#     db_connection.seed("seeds/users.sql")
+#     pass
+
+# def test_all(db_connection):
+#     db_connection.seed("seeds/users.sql")
+#     pass
