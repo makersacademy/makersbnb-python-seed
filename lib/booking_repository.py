5abc8f03f1346800cdcace742cad98eaf89ac89e
JamesLyddon <jameslyddon@gmail.com>
2024-03-05 12:44:35 +0000

    started test driving booking_repository class and added utilities file with string to date converter function

---
+++ lib/booking_repository.py
@@ -0,0 +1,35 @@
+from lib.booking import Booking
+
+class BookingRepository:
+    # We initialise with a database connection
+    def __init__(self, connection):
+        self._connection = connection
+
+    # Retrieve all books
+    def all(self):
+        rows = self._connection.execute('SELECT * from bookings')
+        bookings = []
+        for row in rows:
+            item = Booking(row["id"], row["date"], row["status"], row["space_id"], row["guest_id"])
+            bookings.append(item)
+        return bookings
+
+    # # Find a single book by its id
+    # def find(self, book_id):
+    #     rows = self._connection.execute(
+    #         'SELECT * from books WHERE id = %s', [book_id])
+    #     row = rows[0]
+    #     return Book(row["id"], row["title"], row["author_name"])
+
+    # # Create a new book
+    # # Do you want to get its id back? Look into RETURNING id;
+    # def create(self, book):
+    #     self._connection.execute('INSERT INTO books (title, author_name) VALUES (%s, %s)', [
+    #                              book.title, book.author_name])
+    #     return None
+
+    # # Delete a book by its id
+    # def delete(self, book_id):
+    #     self._connection.execute(
+    #         'DELETE FROM books WHERE id = %s', [book_id])
+    #     return None
---
+++ tests/test_booking_repository.py
@@ -0,0 +1,71 @@
+from lib.booking import Booking
+from lib.booking_repository import BookingRepository
+from lib.utilities import string_to_date
+
+"""
+When we call BookingRepository#all
+We get a list of Book objects reflecting the seed data.
+"""
+def test_get_all_records(db_connection): # See conftest.py to learn what `db_connection` is.
+    db_connection.seed("seeds/bnb_table.sql") # Seed our database with some test data
+    repository = BookingRepository(db_connection) # Create a new BookRepository
+
+    bookings = repository.all() # Get all books
+
+    # Assert on the results
+    assert bookings == [
+        Booking(1, string_to_date('2024-07-13'), True, 1, 2),
+        Booking(2, string_to_date('2024-06-25'), False, 2, 1)
+    ]
+
+# Booking(1, datetime.strptime('2024-07-13', '%Y-%m-%d').date(), True, 1, 2)
+
+# datetime.strptime('2024-07-13', '%Y-%m-%d')
+
+# """
+# When we call BookRepository#find
+# We get a single Book object reflecting the seed data.
+# """
+# def test_get_single_record(db_connection):
+#     db_connection.seed("seeds/book_store.sql")
+#     repository = BookRepository(db_connection)
+
+#     book = repository.find(3)
+#     assert book == Book(3, "Bluets", "Maggie Nelson")
+
+# """
+# When we call BookRepository#create
+# We get a new record in the database.
+# """
+# def test_create_record(db_connection):
+#     db_connection.seed("seeds/book_store.sql")
+#     repository = BookRepository(db_connection)
+
+#     repository.create(Book(None, "The Great Gatsby", "F. Scott Fitzgerald"))
+
+#     result = repository.all()
+#     assert result == [
+#         Book(1, "Invisible Cities", "Italo Calvino"),
+#         Book(2, "The Man Who Was Thursday", "GK Chesterton"),
+#         Book(3, "Bluets", "Maggie Nelson"),
+#         Book(4, "No Place on Earth", "Christa Wolf"),
+#         Book(5, "Nevada", "Imogen Binnie"),
+#         Book(6, "The Great Gatsby", "F. Scott Fitzgerald"),
+#     ]
+
+# """
+# When we call BookRepository#delete
+# We remove a record from the database.
+# """
+# def test_delete_record(db_connection):
+#     db_connection.seed("seeds/book_store.sql")
+#     repository = BookRepository(db_connection)
+#     repository.delete(3) # Apologies to Maggie Nelson fans
+
+#     result = repository.all()
+#     assert result == [
+#         Book(1, "Invisible Cities", "Italo Calvino"),
+#         Book(2, "The Man Who Was Thursday", "GK Chesterton"),
+#         Book(4, "No Place on Earth", "Christa Wolf"),
+#         Book(5, "Nevada", "Imogen Binnie"),
+#     ]
