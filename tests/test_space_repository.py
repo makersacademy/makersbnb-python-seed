60d61b5bb8f0b03070aa0a134cd39fd6ef38c447
Jason <jason.sayeed@gmail.com>
2024-03-05 16:03:36 +0000

    fixes test_space_repo file

--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -1,10 +1,10 @@
 from lib.space_repository import *
 from lib.space import *
 
+
 def test_add_space(db_connection):
     db_connection.seed('seeds/makersbnb.sql')
     repository = SpaceRepository(db_connection)
-
     space = Space(None, 'test description', 99.99, 1, 'test name', '01-01-2024', '31-01-2024')
     print("HERE!!!!!")
     print(space.free_dates)
@@ -17,7 +17,7 @@ def test_all(db_connection):
     db_connection.seed('seeds/makersbnb.sql')
     repository = SpaceRepository(db_connection)
     spaces = repository.all()
-    assert spaces == [Space(1, 'house with a pool', 99.99, 1, 'pool house', date(2024, 3, 4), date(2024, 3, 31)), Space(2, 'house with a garden', 199.99, 2, 'garden house', date(2024, 4, 1), date(2024, 4, 30))]
+    assert spaces == [Space(1, 'house with a pool', 99.99, 1, 'pool house', '04-03-2024', '31-03-2024'), Space(2, 'house with a garden', 199.99, 2, 'garden house', '01-04-2024', '30-04-2024')]
 
 # def test_find():
 #     db_connection.seed('seeds/makersbnb.sql')
