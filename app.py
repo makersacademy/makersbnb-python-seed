ebc9cdf81897930a269db8050d0187afeac66002
Adam Takac <adam.takac13@gmail.com>
2024-03-06 13:55:35 +0000

    resolved conflict

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from markupsafe import escape
 app = Flask(__name__)
 
 # Log in logic
-
 # pass around parameter args <?>
 
 
@@ -82,38 +81,55 @@ if __name__ == '__main__':
 
 
 
-# == Your Routes Here ==
-    def add_space(self, name, description, price_per_night):
-        new_space = {
-            'name': name,
-            'description': description,
-            'price_per_night': price_per_night
-        }
-        self.spaces.append(new_space)
 
-    def view_spaces(self):
-        return self.spaces
 
-    def request_rental(self, space_id, date):
-        # Parameters:
-        #   space_id: int
-        #   date: Date
-        # Returns:
-        #   Nothing. Creates a booking request in database for that specific date
-        pass # No code here yet
 
-    def check_status(self, booking_id):
-        # Parameters:
-        #   booking_id: int
-        # Returns:
-        #   Returns the current status of the booking
-        pass # No code here yet
 
-    def respond(self, booking_id, respond):
-        # Parameters:
-        #   booking_id: int
-        #   response: string
-        # Returns:
-        #   Updates the status of booking in database based on the respond
-        pass # No code here yet
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # == Your Routes Here ==
+#     def add_space(self, name, description, price_per_night):
+#         new_space = {
+#             'name': name,
+#             'description': description,
+#             'price_per_night': price_per_night
+#         }
+#         self.spaces.append(new_space)
+
+#     def view_spaces(self):
+#         return self.spaces
+
+#     def request_rental(self, space_id, date):
+#         # Parameters:
+#         #   space_id: int
+#         #   date: Date
+#         # Returns:
+#         #   Nothing. Creates a booking request in database for that specific date
+#         pass # No code here yet
+
+#     def check_status(self, booking_id):
+#         # Parameters:
+#         #   booking_id: int
+#         # Returns:
+#         #   Returns the current status of the booking
+#         pass # No code here yet
+
+#     def respond(self, booking_id, respond):
+#         # Parameters:
+#         #   booking_id: int
+#         #   response: string
+#         # Returns:
+#         #   Updates the status of booking in database based on the respond
+#         pass # No code here yet
 
