48821475a0250cce0760ee0106502f862d51df66
Matt D <doyle9214@gmail.com>
2024-03-06 10:31:01 +0000

    get request by id and approve request tests started

--- tests/test_request_repository.py
+++ tests/test_request_repository.py
@@ -20,7 +20,22 @@ def test_get_request(db_connection):
     assert request == [
         Request(1, 1, date(2024, 1, 1), 1, 2)
     ]
-# def test_approve_request():
+
+def test_get_request_by_id(db_connection):
+    db_connection.seed('seeds/makersbnb_test.sql')
+    repository = RequestRepository(db_connection)
+    request = repository.get_request_by_id(1)
+    assert request == Request(1, 1, date(2024, 1, 1), 1, 2)
+
+def test_approve_request(db_connection):
+    db_connection.seed('seeds/makersbnb_test.sql')
+    repository = RequestRepository(db_connection)
+    request_id = 2
+    repository.approve_request(request_id)
+    approved_request = repository.get_request_by_id(request_id)
+    assert approved_request.approved == True
+
+
 
 #     pass
 
