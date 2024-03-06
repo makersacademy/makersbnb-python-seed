a5163ff890f0580cecebeae1919b4518a9e085e7
JamesLyddon <jameslyddon@gmail.com>
2024-03-06 08:54:08 +0000

    added more templates and routes

--- app.py
+++ app.py
@@ -10,27 +10,44 @@ app = Flask(__name__)
 
 # == Your Routes Here ==
 
+#== TEST EXAMPLE OF GETTING DATA FROM THE DATABASE USING CONNECTION AND REPOSITORY ==#
+@app.route("/test", methods=["GET"]) # route an method/s e.g. browser send GET request to website.com/test
+def get_test():
+    connection = get_flask_database_connection(app) # set up connection to database on local machine
+    repository = UserRepository(connection) # create a user_repo instance so we can use its methods
+    users = repository.all() # use the #all method from user_repo and save in the variable 'users'
+    return render_template('test.html', users=users) # return an html template and pass the template the users data
+#====================================================================================#
+
+
 # Sign-up form page
 @app.route('/', methods=['GET'])
 def get_index():
+    connection = get_flask_database_connection(app)
     return render_template('sign-up.html')
 
 # Sign-up POST request
 @app.route("/", methods=["POST"])
 def post_index():
+    connection = get_flask_database_connection(app)
     # We extract the message from the request
     email = request.form.get("email")
     password = request.form.get("password")
     confirm_password = request.form.get("confirm-password")
 
-# TEST EXAMPLE OF GETTING DATA FROM THE DATABASE USING CONNECTION AND REPOSITORY
-@app.route("/test", methods=["GET"])
-def get_test():
+
+
+@app.route("/book-space", methods=["GET", "POST"])
+def get_spaces():
     connection = get_flask_database_connection(app)
-    repository = UserRepository(connection)
-    users = repository.all()
-    return render_template('test.html', users=users)
-#===============================================================================
+    # TODO get all spaces
+    return render_template('book-space.html')
+
+@app.route("/list-space", methods=["GET", "POST"])
+def list_space():
+    conection = get_flask_database_connection(app)
+    # TODO get data from form and create new space in db
+    return render_template('list-space.html')
 
 
 
