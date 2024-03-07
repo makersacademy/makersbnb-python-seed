86a3875910654b908e1fbab6d71040ba42f07ae5
Mohammad Hassan Azad <m.h.azad-14@outlook.com>
2024-03-07 11:22:43 +0000

    HTML booking page frontend  done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.property import Property
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -14,6 +15,12 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+# Placeholder for property booking
+@app.route('/properties/<int:id>', methods=['GET'])
+def show_property_by_id(id):
+    property = Property(1, "Burnaston Road", 1, "hot", 20.50)
+    return render_template('get_property.html', property=property)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,21 @@ def test_get_index(page, test_web_address):
 
     # We assert that it has the text "This is the homepage."
     expect(strong_tag).to_have_text("This is the blueberries b&b homepage.")
+
+"""
+Test property information and booking page loads and has input fields with labels
+"""
+
+def test_get_property(page, test_web_address, db_connection):
+    db_connection.seed("seeds/blueberries_bnb.sql")
+    page.goto(f"http://{test_web_address}/properties/1")
+
+    h1_tag = page.locator("h1")
+    inputs = page.locator("input")
+    labels = page.locator("label")
+    button = page.get_by_role("button")
+
+    expect(h1_tag).to_have_text("Burnaston Road")
+    expect(inputs).to_have_count(3)
+    expect(labels).to_have_count(2)
+    expect(button).to_have_count(1)
