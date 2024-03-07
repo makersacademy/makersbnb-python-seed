6b27a94eea80616d69d6e7e028be5f95ff90bdad
Arihant <ari.jain2410@gmail.com>
2024-03-07 13:00:17 +0000

    index, app & test_app

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from lib.database_connection import get_flask_database_connection
 app = Flask(__name__)
 
 # == Your Routes Here ==
+properties_name = [
+    {"name": "Amazing sea views", "location": "Canary Islands", "price": "£400 a night", "description": "Minutes from the beach, perfect for early morning walks and watching the besutiful sunset in the evenings."},
+    {"name": "Location, Location, Location...", "location": "London", "price": "£1000 a night", "description": "close to central London and its ameneties"},
+    {"name": "Earthen home", "location": "Terrasini, Italy", "price": "$1,000,000", "description": "A sophisticated chest, cozy, immersed in nature and unmistakable style."}
+]
 
 # GET /index
 # Returns the homepage
@@ -13,7 +18,7 @@ app = Flask(__name__)
 #   ; open http://localhost:5001/index
 @app.route('/index', methods=['GET'])
 def get_index():
-    return render_template('index.html')
+    return render_template('index.html', properties=properties_name)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
