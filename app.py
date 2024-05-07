ca021eaa7653b096d9f4c19472a6ccb5328f63af
Melissa Deeney <melissadeeney@gmail.com>
2024-05-07 15:29:13 +0100

    testing register page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 from lib.database_connection import get_flask_database_connection
 
 # Create a new Flask app
@@ -14,6 +14,16 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        db_connection = get_flask_database_connection(app)
+        username = request.form['username']
+        password = request.form['password']
+        db_connection.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+        return redirect(url_for('login'))
+    return render_template('register.html')
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
