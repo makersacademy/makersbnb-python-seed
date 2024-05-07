9af2608f566a7eaaf829106f9a59b59d4d48f614
Jetheesan <jetheesan@gmail.com>
2024-05-07 17:29:42 +0100

    Adding routes to app.py and creating html templates

--- app.py
+++ app.py
@@ -15,6 +15,44 @@ app = Flask(__name__)
 def get_index():
     return render_template('index.html')
 
+@app.route('/', methods=['GET'])
+def get_home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET'])
+def get_login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    # to check if login information is valid we'll need user.py     
+    return render_template('login.html')
+
+@app.route('/about', methods=['GET'])
+def get_about():
+    return render_template('about.html')
+
+@app.route('/spaces', methods=['POST'])
+def post_spaces():
+    return render_template('spaces.html')
+
+@app.route('/spaces', methods=['GET'])
+def get_spaces():
+    return render_template('spaces.html')
+
+@app.route('/spaces/new', methods=['POST'])
+def create_a_space():
+    return render_template('create_space.html')
+
+@app.route('/requests', methods=['GET'])
+def get_requests():
+    return render_template('requests.html')
+
+    
+
+
+
+
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
 # if started in test mode.
