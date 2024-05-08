989b9418ed847efec36ea07d25f98f6a3ef2fec3
Jetheesan <jetheesan@gmail.com>
2024-05-08 15:07:22 +0100

    updating app.py and html with ID

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from lib.database_connection import get_flask_database_connection
 
 # Create a new Flask app
 app = Flask(__name__)
+# global variable 
+id = 1
 
 # == Your Routes Here ==
 
@@ -32,28 +34,28 @@ def login():
 def get_about():
     return render_template('about.html')
 
-@app.route('/spaces', methods=['POST'])
+@app.route('/1/spaces', methods=['POST'])
 def post_spaces():
     return render_template('spaces.html')
 
-@app.route('/spaces', methods=['GET'])
+@app.route('/1/spaces', methods=['GET'])
 def get_spaces():
-    return render_template('spaces.html')
+    return render_template('spaces.html' )
 
-@app.route('/spaces/new', methods=['POST'])
+@app.route('/1/spaces/new', methods=['POST'])
 def create_a_space():
     return render_template('create_space.html')
 
-@app.route('/requests', methods=['GET'])
+@app.route('/1/requests', methods=['GET'])
 def get_requests():
     return render_template('requests.html')
 
-@app.route('/requests', methods=['POST'])
+@app.route('/1/requests', methods=['POST'])
 def post_requests():
     return render_template('requests.html')
 
 
-@app.route('/spaces/1' , methods=(['GET']))  #change 1 to id later
+@app.route('/1/spaces/1' , methods=(['GET']))  #change 1 to id later
 def post_request_space():
     return render_template('single_space.html')
 
