77d35556fe10f9a6a6f231eece94c138a01c44df
Jack <jackawmurray@hotmail.com>
2024-05-09 15:39:59 +0100

    Changed db for requests

--- seeds/seed.sql
+++ seeds/seed.sql
@@ -33,6 +33,7 @@ CREATE TABLE users_spaces_requests (
   space_id int,
   start_date date,
   end_date date,
+  approval text
   constraint fk_user foreign key(user_id) references users(id) on delete cascade,
   constraint fk_space foreign key(space_id) references spaces(id) on delete cascade,
   PRIMARY KEY (user_id, space_id)
@@ -48,7 +49,7 @@ INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_d
 INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (2, 'venue #4', 'desc #4', 80, '2024-01-03', '2024-02-03');
 INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (2, 'venue #5', 'desc #5', 90, '2024-02-04', '2024-02-05');
 
-INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (1, 2, '2024-01-01', '2024-01-08');
-INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (2, 3, '2024-04-04', '2024-05-05');
-INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (3, 4, '2024-01-01', '2024-01-05');
-INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (2, 4, '2024-01-03', '2024-02-03');
+-- INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (1, 2, '2024-01-01', '2024-01-08');
+-- INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (2, 3, '2024-04-04', '2024-05-05');
+-- INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (3, 4, '2024-01-01', '2024-01-05');
+-- INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (2, 4, '2024-01-03', '2024-02-03');
