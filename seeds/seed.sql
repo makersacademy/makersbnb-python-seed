cf3979199940e2048690c1f04ec9a30b1dc46818
niamh <niamh321@me.com>
2024-05-09 16:15:14 +0100

    updated name and description data in seed file.

--- seeds/seed.sql
+++ seeds/seed.sql
@@ -43,11 +43,11 @@ INSERT INTO users (name, email, password) VALUES ('Person1', 'test1@gmail.com', 
 INSERT INTO users (name, email, password) VALUES ('Person2', 'test2@gmail.com', 'password123');
 INSERT INTO users (name, email, password) VALUES ('Person3', 'test3@gmail.com', 'password123'); 
 
-INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (1, 'venue #1', 'desc #1', 50, '2024-01-01', '2024-01-08');
-INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (1, 'venue #2', 'desc #2', 60, '2024-04-04', '2024-05-05');
-INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (1, 'venue #3', 'desc #3', 70, '2024-01-01', '2024-01-05');
-INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (2, 'venue #4', 'desc #4', 80, '2024-01-03', '2024-02-03');
-INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (2, 'venue #5', 'desc #5', 90, '2024-02-04', '2024-02-05');
+INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (1, 'Entire villa in Groombridge', 'A light, warm, and modern space for a gathering.  Wonderful outdoor living.  Beautiful gardens in Area of Outstanding Natural Beauty in the High Weald .', 50, '2024-01-01', '2024-01-08');
+INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (1, 'Farm stay in Kent', 'Private cosy traditional log cabin on the lake, surrounded by beautiful  countryside. Lovely peaceful get away from it all.', 60, '2024-04-04', '2024-05-05');
+INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (1, 'Entire home in Saint Margarets Bay', 'The most stunning location of any house on the South Coast. Wrap around sea iews', 70, '2024-01-01', '2024-01-05');
+INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (2, 'Entire rental unit in Berck, France', 'Come and enjoy this hidden gem facing the Berckoan beach and contemplate the unique sunsets of the Opal Coast.', 80, '2024-01-03', '2024-02-03');
+INSERT INTO spaces (owner, name, description, price_per_night, start_date, end_date) VALUES (2, 'Beautiful loft in Paris, France', 'Unique rooftop with 360 views of Paris and its monuments, including a magnificent unobstructed view of the Eiffel Tower.', 90, '2024-02-04', '2024-02-05');
 
 -- INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (1, 2, '2024-01-01', '2024-01-08');
 -- INSERT INTO users_spaces_requests (user_id, space_id, start_date, end_date) VALUES (2, 3, '2024-04-04', '2024-05-05');
