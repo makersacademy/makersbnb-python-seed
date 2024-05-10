7440d58c0fd5d1bc37e4bf3f32a542e4ed8e0d40
Andrew Zarei <andrew.zarei.09@gmail.com>
2024-05-10 11:38:36 +0100

    bug fix on booking routes

--- lib/booking_routes.py
+++ lib/booking_routes.py
@@ -15,6 +15,7 @@ def apply_booking_routes(app):
         
         # Fetch booking requests made by the user
         user_bookings = booking_repository.get_bookings_with_space_names(user_id)
+        # print(user_bookings)
         user_booking_requests = [(booking, space_name, confirmed) for booking, space_name, confirmed in user_bookings]
         
         # Fetch booking requests received by the user
@@ -22,9 +23,9 @@ def apply_booking_routes(app):
         requests_received = []
         for space in user_space_listings:
             space_bookings = booking_repository.get_bookings_by_space(space.id)
-            for booking in space_bookings:
-                booking_obj, space_name, confirmed = booking
-                requests_received.append((booking_obj, space_name, confirmed))
+            if len(space_bookings[0]) > 1:
+                for booking in space_bookings:
+                    requests_received.append((booking[0], booking[1], booking[2]))
                 
         return render_template('bookings.html', user_booking_requests=user_booking_requests, requests_received=requests_received)
 
