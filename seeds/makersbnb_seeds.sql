0310847c424ef1733cfa12f2cbcd0332dde00237
Marya <ms930652210@googlemail.com>
2024-07-08 15:45:42 +0100

    sql seeds file for our tables

---
+++ seeds/makersbnb_seeds.sql
@@ -0,0 +1,36 @@
+-- The job of this file is to reset all of our important database tables.
+-- And add any data that is needed for the tests to run.
+-- This is so that our tests, and application, are always operating from a fresh
+-- database state, and that tests don't interfere with each other.
+
+-- First, we must delete (drop) all our tables
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS users_id_seq;
+
+DROP TABLE IF EXISTS listings;
+DROP SEQUENCE IF EXISTS listings_id_seq;
+
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+    username text
+);
+-- Then, we recreate them
+CREATE SEQUENCE IF NOT EXISTS listings_id_seq;
+CREATE TABLE listings (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    price_per_night FLOAT(10),
+    available_from DATE, 
+    available_to DATE,
+    user_id int,
+    constraint fk_user foreign key(user_id)
+    references users(id)
+    on delete cascade
+);
+
+-- Finally, we add any records that are needed for the tests to run
+INSERT INTO users (username) VALUES ('marya_test');
+
+INSERT INTO listings (name, description, price_per_night, available_from, available_to, user_id) VALUES ('test-cottage', 'nice cottage by the beach', 25.00, '2024-01-01', '2024-10-01', 1)
