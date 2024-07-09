29e2eab8956d2ea25d986fc6b0401350ef66c9c5
Reza Jugon <rezajugon@icloud.com>
2024-07-09 11:08:00 +0100

    Added routes for the top 4 screens.

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 
 # == Your Routes Here ==
 
+# http://127.0.0.1:5001/index
+# http://127.0.0.1:5001/spaces
+# http://127.0.0.1:5001/spaces/new
+# http://127.0.0.1:5001/requests
+
+
 # GET /index
 # Returns the homepage
 # Try it:
@@ -14,6 +20,22 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+# List available spaces.
+@app.route('/spaces', methods=['GET'])
+def get_spaces():
+    return render_template('spaces.html')
+
+# List a new space as a Property owner.
+@app.route('/spaces/new', methods=['GET'])
+def list_new_property():
+    return render_template('spaces_new.html')
+
+# See requests that I've made and received so far.
+@app.route('/requests', methods=['GET'])
+def get_requests():
+    return render_template('requests.html')
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
