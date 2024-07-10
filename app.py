da339f9e4172d2b08444a14bedbacd335f8ced7a
Tara <taconlin@hotmail.co.uk>
2024-07-10 17:02:42 +0100

    Updated HTML for /spaces and /spaces/new

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from lib.database_connection import get_flask_database_connection
-
+from lib.PropertyRepository import PropertyRepository
 from lib.BookingRequestRepository import BookingRequestRepository
+from lib.Property import Property
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -42,6 +43,35 @@ def get_requests():
     repository = BookingRequestRepository(Connection)
     bookings_list = repository.all()
     return render_template('requests.html', bookings_list = bookings_list)
+
+
+# POST /spaces/new
+    # Creates a new book
+@app.route('/spaces/new', methods=['POST'])
+def create_space():
+    # Set up the database connection and repository
+    connection = get_flask_database_connection(app)
+    repository = PropertyRepository(connection)
+
+    # Get the fields from the request form
+    property = request.form['Property Name']
+    description = request.form['Description']
+    price_per_night = request.form['Price per Night (£)']
+    available_from = request.form['Available From (dd-mm-yy)']
+    available_to = request.form['Available To (dd-mm-yy)']
+
+    # Create a property object
+    property = Property(None, property, description, price_per_night, available_from, available_to)
+
+    # Check for validity and if not valid, show the form again with errors
+    if not property.is_valid():
+        return render_template('spaces/new.html', property=property, errors=property.generate_errors()), 400
+
+    # Save the property to the database
+    property = repository.create(property)
+
+    # Redirect to the book's show route to the user can see it
+    return redirect(f"/spaces/{property.id}")
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
